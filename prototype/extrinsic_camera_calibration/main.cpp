968c923fa963bc79c5b6354ec79a1139373869f3
janismac <Janis.Maczijewski@rwth-acchen.de>
2017-10-23 17:17:28 +0200

    refactoring

--- prototype/extrinsic_camera_calibration/main.cpp
+++ prototype/extrinsic_camera_calibration/main.cpp
@@ -4,39 +4,63 @@
 #include "apriltag/apriltag.h"
 #include "opencv2/opencv.hpp"
 #include "AprilTagDetector.h"
+#include "Parameter.h"
 
 #include <iostream>
 
 
-
-
-cv::Point2d reproject(cv::Point3d point, cv::Matx33d CameraR, cv::Vec3d CameraT, double fx, double fy, double px, double py, double k1, double k2, double p1, double p2) {
-
-
-    cv::Matx31d p = CameraR * cv::Matx31d(point.x, point.y, point.z) + CameraT;
-
-
-    double x = p(0, 0);
-    double y = p(1, 0);
-    double z = p(2, 0);
-    assert(z > 0);
-    x /= z;
-    y /= z;
-    double r2 = x * x + y * y;
-    double r4 = r2 * r2;
-    double x2 = x * (1 + k1 * r2 + k2 * r4) + 2 * p1 * x * y + p2 * (r2 + 2 * x * x);
-    double y2 = y * (1 + k1 * r2 + k2 * r4) + p1 * (r2 + 2 * y * y) + 2 * p2 * x * y;
-    double u = fx * x2 + px;
-    double v = fy * y2 + py;
-
-    return cv::Point2d(u,v);
-}
+struct CameraParameters {
+    Parameter<cv::Matx33d> R;
+    Parameter<cv::Vec3d> T;
+    Parameter<double> fx, fy, cx, cy, k1, k2, k3, p1, p2;
 
 
 
+    cv::Point2d reproject(cv::Point3d point) {
+        cv::Matx31d p = R() * cv::Matx31d(point.x, point.y, point.z) + T();
+
+        double x = p(0, 0);
+        double y = p(1, 0);
+        double z = p(2, 0);
+        assert(z > 0);
+        x /= z;
+        y /= z;
+
+        double x_sq = x * x;
+        double y_sq = y * y;
+        double r_sq = x_sq + y_sq;
+        double r4 = r_sq * r_sq;
+        double r6 = r4 * r_sq;
+
+        double x2 = x * (1 + k1 * r_sq + k2 * r4 + k3 * r6) + 2 * p1 * x * y + p2 * (r_sq + 2 * x_sq);
+        double y2 = y * (1 + k1 * r_sq + k2 * r4 + k3 * r6) + p1 * (r_sq + 2 * y_sq) + 2 * p2 * x * y;
+        double u = fx * x2 + cx;
+        double v = fy * y2 + cy;
+
+        return cv::Point2d(u,v);
+    }
 
 
-void getRelativeTransform(cv::Mat cv_img, std::vector<AprilTagDetection> detections, double fx, double fy, double px, double py, double k1, double k2, double p1, double p2) {
+    void setExtrinsicsFromPnP(std::vector<cv::Point3d> objPts, std::vector<cv::Point2d> imgPts) {
+        cv::Vec3d rvec,t;
+        cv::Matx33d r;
+        cv::Matx33d cameraMatrix(
+                fx(), 0, cx(),
+                0, fy(), cy(),
+                0,  0,  1);
+        cv::Vec4d distParam(k1(), k2(), p1(), p2());
+        cv::solvePnP(objPts, imgPts, cameraMatrix, distParam, rvec, t);
+        cv::Rodrigues(rvec, r);
+        T = t;
+        R = r;
+    }
+
+
+};
+
+
+
+void calculateRelativeTransform(std::vector<AprilTagDetection> detections, CameraParameters &params) {
     std::vector<cv::Point3d> objPts;
     std::vector<cv::Point2d> imgPts;
 
@@ -51,6 +75,7 @@ void getRelativeTransform(cv::Mat cv_img, std::vector<AprilTagDetection> detecti
     }
 
 
+    // Manually labelled
     objPts.push_back(cv::Point3d(3,3,0));
     imgPts.push_back(cv::Point2d(993,223));
 
@@ -60,52 +85,8 @@ void getRelativeTransform(cv::Mat cv_img, std::vector<AprilTagDetection> detecti
     objPts.push_back(cv::Point3d(3,0,0));
     imgPts.push_back(cv::Point2d(1482,638));
 
-    /*objPts.push_back(cv::Point3d(0,0,0));
-    imgPts.push_back(cv::Point2d(229,930));
 
-    objPts.push_back(cv::Point3d(1,1,0));
-    imgPts.push_back(cv::Point2d(781,422));*/
-
-
-
-
-    cv::Vec3d rvec, tvec;
-    cv::Matx33d cameraMatrix(
-            fx, 0, px,
-            0, fy, py,
-            0,  0,  1);
-    cv::Vec4d distParam(k1, k2, p1, p2);
-    cv::solvePnP(objPts, imgPts, cameraMatrix, distParam, rvec, tvec);
-    cv::Matx33d r;
-    cv::Rodrigues(rvec, r);
-
-    std::cout << "r" << std::endl;
-    std::cout << r << std::endl;
-    std::cout << "t" << std::endl;
-    std::cout << tvec << std::endl;
-
-    cvtColor(cv_img, cv_img, cv::COLOR_GRAY2BGR);
-
-
-    {
-        // Reprojection
-        std::cout << "Reprojection" << std::endl;
-        for (int x = 0; x < 4; ++x) {
-            for (int y = 0; y < 4; ++y) {
-                auto pt1 = reproject(cv::Point3d(x,y,0), r, tvec, fx, fy, px, py, k1, k2, p1, p2);
-                auto pt2 = reproject(cv::Point3d(x,y,0.4), r, tvec, fx, fy, px, py, k1, k2, p1, p2);
-
-                //cv::circle(cv_img, pt_2d, 20, cv::Scalar(255), 3);
-                cv::line(cv_img, pt1, pt2, cv::Scalar(0,0,255),4);
-            }
-        }
-        cv::namedWindow("a");
-        cv::imwrite("CameraCalibrationDemo.png", cv_img);
-        cv::imshow("a",cv_img);
-        cv::waitKey(0);
-
-    }
-
+    params.setExtrinsicsFromPnP(objPts, imgPts);
 
 }
 
@@ -133,17 +114,46 @@ int main(int argc, char *argv[])
             );
         }
 
-        double fx = 945.444618;
-        double fy = 898.791897;
-        double px = 776.656112;
-        double py = 533.951952;
-        double k1 = -0.253795;
-        double k2 = 0.041498;
-        double p1 = 0.019698;
-        double p2 = 0.001308;
+        CameraParameters params;
+
+        params.fx = 945.444618;
+        params.fy = 898.791897;
+        params.cx = 776.656112;
+        params.cy = 533.951952;
+        params.k1 = -0.253795;
+        params.k2 = 0.041498;
+        params.k3 = 0;
+        params.p1 = 0.019698;
+        params.p2 = 0.001308;
+
+        calculateRelativeTransform(detections, params);
 
 
-        getRelativeTransform(cv_img, detections, fx, fy, px, py, k1, k2, p1, p2);
+        cvtColor(cv_img, cv_img, cv::COLOR_GRAY2BGR);
+
+
+
+        for (int x = 0; x < 4; ++x) {
+            for (int y = 0; y < 3; ++y) {
+                cv::line(cv_img, params.reproject(cv::Point3d(x,y + .2,0)), params.reproject(cv::Point3d(x,y + .8,0)), cv::Scalar(0,255,0),4);
+                cv::line(cv_img, params.reproject(cv::Point3d(y + .2,x,0)), params.reproject(cv::Point3d(y + .8,x,0)), cv::Scalar(0,0,255),4);
+
+            }
+        }
+
+
+
+        for (int x = 0; x < 4; ++x) {
+            for (int y = 0; y < 4; ++y) {
+                auto pt1 = params.reproject(cv::Point3d(x,y,0));
+                auto pt2 = params.reproject(cv::Point3d(x,y,0.4));
+                cv::line(cv_img, pt1, pt2, cv::Scalar(255,0,0),4);
+            }
+        }
+        cv::namedWindow("a");
+        cv::imwrite("CameraCalibrationDemo.png", cv_img);
+        cv::imshow("a",cv_img);
+        cv::waitKey(0);
     }
 
     return 0;
