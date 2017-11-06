640d4f221ed2b5f248cc8ee143e0c93affb203f2
janismac <Janis.Maczijewski@rwth-acchen.de>
2017-11-06 18:50:24 +0100

    documentation

--- ROS/src/ips/src/algorithms/AprilTagDetector/AprilTagDetector.h
+++ ROS/src/ips/src/algorithms/AprilTagDetector/AprilTagDetector.h
@@ -10,23 +10,37 @@
 #include <array>
 #include <vector>
 
+/*!
+    Represents the detection of a tag.
+*/
 struct AprilTagDetection {
-    // Point indices
+
+    //@{
+    /*! Index name for the AprilTagDetection::points array. */
     static constexpr int i_bottom_left = 0;
     static constexpr int i_bottom_right = 1;
     static constexpr int i_top_right = 2;
     static constexpr int i_top_left = 3;
     static constexpr int i_center = 4;
+    //@}
 
+    /*! (x,y) pixel coordinates for the tag corners and center. */
     std::array<std::array<double, 2>, 5> points;
+
+    //@{
+    /*! See <a href="file:///usr/local/include/apriltag/apriltag.h">apriltag.h</a>  */
     int id;
     int hamming;
     float goodness;
     float decision_margin;
+    //@}
 };
 
 enum class AprilTagFamily { Tag36h11, Tag36h10, Tag25h9, Tag25h7, Tag16h5 };
 
+/*!
+    Thin wrapper class for the apriltag detector C library.
+*/
 class AprilTagDetector {
 
     apriltag_family_t *tf;
@@ -34,8 +48,10 @@ class AprilTagDetector {
     AprilTagFamily family;
 
 public:
-    AprilTagDetector(AprilTagFamily);
+    /*! \param family The used tag family. */
+    AprilTagDetector(AprilTagFamily family);
     ~AprilTagDetector();
 
+    /*! Find apriltags in a black and white image. */
     std::vector<AprilTagDetection> detect(image_u8_t im);
 };
