e50d97b7bb9adb1ddf3d2ed7193db525585205fb
janismac <Janis.Maczijewski@rwth-acchen.de>
2017-11-07 16:02:17 +0100

    improved clion setup script

--- ROS/clion.sh
+++ ROS/clion.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+set -e
 
 source setup.sh
+
+if [ ! -f src/.idea/workspace.xml ]; then
+    cp src/.idea/workspace.xml.template src/.idea/workspace.xml
+fi
+
 nohup ~/clion/bin/clion.sh src >/dev/null 2>&1 &
--- ROS/setup.sh
+++ ROS/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 clear
 source /opt/ros/kinetic/setup.bash
