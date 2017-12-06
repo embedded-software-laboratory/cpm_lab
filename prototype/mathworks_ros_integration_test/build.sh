01b7735b81c9399da2063fa909908b9185d09a9f
janismac <Janis.Maczijewski@rwth-acchen.de>
2017-12-06 18:37:26 +0100

    WIP mathworks_ros_integration_test

--- prototype/mathworks_ros_integration_test/build.sh
+++ prototype/mathworks_ros_integration_test/build.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 clear
+kill $(jobs -p)
 source /opt/ros/kinetic/setup.bash
 roscore &
+/opt/MATLAB/R2017b/bin/matlab -nodisplay -nodesktop -r "cd('src/controller_in_simulink/'); load_system('controller'); rtwbuild('controller'); exit"
+rm -rf src/controller_in_simulink/controller_ert_rtw/
+src/controller_in_simulink/build_ros_model.sh src/controller_in_simulink/controller.tgz $(pwd)
+catkin_make
+. devel/setup.bash
+kill $(jobs -p)
+sleep 3
 
-
-cd('src/controller_in_simulink/'); load_system('controller_in_simulink'); rtwbuild('controller_in_simulink'); exit
-
-load_system('controller_in_simulink')
+roscore &
+rosrun controller controller_node &
+rosrun reference_position_in_cpp reference_position_in_cpp &
+rqt_plot position reference &
+/opt/MATLAB/R2017b/bin/matlab -nodisplay -nodesktop -r "cd('src/simulation_in_matlab/'); main;" </dev/null >/dev/null &
