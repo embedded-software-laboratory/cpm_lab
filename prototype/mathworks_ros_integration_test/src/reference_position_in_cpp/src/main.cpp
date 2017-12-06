f8af46d0922ecc50c62afe5870a6c0c7de5069b3
janismac <Janis.Maczijewski@rwth-acchen.de>
2017-12-06 14:34:44 +0100

    WIP mathworks_ros_integration_test

--- prototype/mathworks_ros_integration_test/src/reference_position_in_cpp/src/main.cpp
+++ prototype/mathworks_ros_integration_test/src/reference_position_in_cpp/src/main.cpp
@@ -4,17 +4,18 @@
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv);
+    ros::init(argc, argv, "reference_position_in_cpp");
     ros::NodeHandle n;
     ros::Publisher ref_publisher = n.advertise<std_msgs::Float64>("reference", 1);
-    ros::Rate loop_rate(10);
+    ros::Rate loop_rate(50);
     int count = 0;
     while (ros::ok())
     {
         std_msgs::Float64 msg;
-        msg.data = 5;
+        msg.data = (count/20)%4+1;
         ref_publisher.publish(msg);
         loop_rate.sleep();
+        count++;
     }
 
     return 0;
