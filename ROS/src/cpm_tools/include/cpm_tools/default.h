48dd1a23edc25ca7a38aa3afc007093230f5eea6
janismac <Janis.Maczijewski@rwth-acchen.de>
2017-12-15 11:30:06 +0100

    WIP splitting generic parts from ips to cpm_tools package

---
+++ ROS/src/cpm_tools/include/cpm_tools/default.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <tuple>
+#include <memory>
+#include <thread>
+#include <cmath>
+#include <iostream>
+#include <ros/time.h>
+#include <limits>
+#include <map>
+#include <experimental/optional>
+
+using std::vector;
+using std::string;
+using std::to_string;
+using std::tuple;
+using std::make_tuple;
+using std::tie;
+using std::shared_ptr;
+using std::make_shared;
+using std::cout;
+using std::endl;
+using std::thread;
+using std::map;
+using std::get;
+using std::experimental::optional;
+
+constexpr double NaN = std::numeric_limits<double>::quiet_NaN();
+
+template <typename Base>
+struct WithTimestamp : public Base {
+    ros::Time timestamp;
+    WithTimestamp(){}
+    WithTimestamp(const Base &base, ros::Time timestamp):Base(base),timestamp(timestamp){}
+    WithTimestamp(Base &&base, ros::Time timestamp):Base(std::move(base)),timestamp(timestamp){}
+};
