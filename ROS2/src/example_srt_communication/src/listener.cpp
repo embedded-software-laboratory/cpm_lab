8922f8511fc04e1297a5a922c6e5aebabfd7f8d1
janismac <Janis.Maczijewski@rwth-acchen.de>
2018-02-09 16:00:17 +0100

    SubscriptionBuffer moved to new package cpm_tools

--- ROS2/src/example_srt_communication/src/listener.cpp
+++ ROS2/src/example_srt_communication/src/listener.cpp
@@ -1,37 +1,9 @@
 #include <chrono>
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "cpm_tools/SubscriptionBuffer.hpp"
 using std::placeholders::_1;
 using namespace std::chrono_literals;
-
-template <typename MessageT, int buffer_size>
-class SubscriptionBuffer {
-    MessageT buffer[buffer_size];
-    size_t latest_message_index = 0;
-    typename rclcpp::Subscription<MessageT>::SharedPtr subscription_;
-    std::function<void()> new_message_callback_;
-
-    void topic_callback(const typename MessageT::SharedPtr msg) {
-        // save messages in reverse chronologic order
-        latest_message_index = (buffer_size + latest_message_index - 1) % buffer_size;
-        buffer[latest_message_index] = *msg;
-        if(new_message_callback_)new_message_callback_();
-    }
-
-public:
-    SubscriptionBuffer (const std::string &topic_name, rclcpp::Node* node, std::function<void()> new_message_callback)
-    : subscription_(node->create_subscription<MessageT>(
-        topic_name, std::bind(&SubscriptionBuffer::topic_callback, this, _1), rmw_qos_profile_sensor_data
-    )),
-    new_message_callback_(new_message_callback)
-    { }
-
-    size_t size() { return buffer_size; }
-
-    const MessageT& at(size_t i) {
-        return buffer[(latest_message_index + i) % buffer_size];
-    }
-};
 
 
 class Listener : public rclcpp::Node {
