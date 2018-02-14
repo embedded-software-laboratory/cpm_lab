4916846a46e3c6b34100c44eecb7f5c43320ab02
janismac <Janis.Maczijewski@rwth-acchen.de>
2018-02-14 18:54:13 +0100

    WIP CPM ROS2 node structure

--- ROS2/src/cpm_tools/include/cpm_tools/Subscriber.hpp
+++ ROS2/src/cpm_tools/include/cpm_tools/Subscriber.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include "rclcpp/rclcpp.hpp"
 using std::placeholders::_1;
 
--- ROS2/src/example_srt_communication/src/listener.cpp
+++ ROS2/src/example_srt_communication/src/listener.cpp
@@ -6,90 +6,11 @@
 #include "cpm_msgs/msg/vehicle_sensors.hpp"
 #include "cpm_tools/BinarySemaphore.hpp"
 #include "cpm_tools/AbsoluteTimer.hpp"
+#include "cpm_tools/CpmNode.hpp"
 
-using std::placeholders::_1;
 using namespace std::chrono_literals;
 using namespace cpm_tools;
 
-#define NANOSEC_PER_SEC 1000000000ull
-
-uint64_t clock_gettime_nanoseconds() {
-    struct timespec t;
-    clock_gettime(CLOCK_REALTIME, &t);
-    return uint64_t(t.tv_sec) * NANOSEC_PER_SEC + uint64_t(t.tv_nsec);
-}
-
-
-class CpmNode : public rclcpp::Node {
-private:
-    uint64_t period_nanoseconds_; 
-    uint64_t offset_nanoseconds_;
-    bool allow_early_execution_;
-    BinarySemaphore semaphore;
-    std::vector<SubscriberBase::SharedPtr> subscribers;
-    AbsoluteTimer absoluteTimer_;
-public:
-    CpmNode(const std::string &node_name, uint64_t period_nanoseconds, uint64_t offset_nanoseconds, bool allow_early_execution)
-    :Node(node_name)
-    ,period_nanoseconds_(period_nanoseconds)
-    ,offset_nanoseconds_(offset_nanoseconds)
-    ,allow_early_execution_(allow_early_execution)
-    ,absoluteTimer_(
-        time_t(period_nanoseconds / NANOSEC_PER_SEC),
-        long(period_nanoseconds % NANOSEC_PER_SEC),
-        time_t(offset_nanoseconds / NANOSEC_PER_SEC),
-        long(offset_nanoseconds % NANOSEC_PER_SEC),
-        [this](){ semaphore.signal(); }
-    ){}
-
-    uint64_t getPeriodNanoseconds() {return period_nanoseconds_;}
-    uint64_t getOffsetNanoseconds() {return offset_nanoseconds_;}
-    bool allowEarlyExecution() {return allow_early_execution_;}
-
-    void start_loop() {
-        // Set the first deadline to the next full second plus offset
-        // The period must divide one second for this to work reliably across a distributed system (TODO)
-        uint64_t deadline_nanoseconds = ((clock_gettime_nanoseconds()/NANOSEC_PER_SEC)+1)*NANOSEC_PER_SEC + offset_nanoseconds_;
-        while(rclcpp::ok()) {
-            semaphore.wait();
-
-            bool deadline_arrived = clock_gettime_nanoseconds() >= deadline_nanoseconds;
-
-            bool all_messages_arrived = true;
-            for(auto subscriber : subscribers) {
-                if(!subscriber->message_arrived(deadline_nanoseconds)) {
-                    all_messages_arrived = false;
-                    break;
-                }
-            }
-
-            if(deadline_arrived && !all_messages_arrived) {
-                RCLCPP_WARN(this->get_logger(), "New messages missing, using old messages, in node '%s'", get_name())
-            }
-
-            if(deadline_arrived || (allow_early_execution_ && all_messages_arrived)) {
-                RCLCPP_INFO(this->get_logger(), "starting run() for deadline %llu at time %llu", deadline_nanoseconds, clock_gettime_nanoseconds())
-                this->update(deadline_nanoseconds);
-                deadline_nanoseconds += period_nanoseconds_;
-                while(clock_gettime_nanoseconds() >= deadline_nanoseconds) {
-                    RCLCPP_WARN(this->get_logger(), "Missed deadline %llu", deadline_nanoseconds)
-                    deadline_nanoseconds += period_nanoseconds_;
-                }
-            }
-        }
-    }
-
-    template<typename T>
-    typename cpm_tools::Subscriber<T>::SharedPtr 
-    subscribe(const std::string &topic_name, uint64_t expected_delay) {
-        auto sub = std::make_shared<Subscriber<T>>(topic_name, expected_delay, this, [this](){ semaphore.signal(); });
-        subscribers.push_back(sub);
-        return sub;
-    }
-
-    virtual void update(uint64_t deadline_nanoseconds) = 0;
-    virtual ~CpmNode() {}
-};
 
 class ListenerNode : public CpmNode {
     Subscriber<cpm_msgs::msg::VehicleSensors>::SharedPtr subscriber_vehicle_sensors;
@@ -127,8 +48,4 @@ int main(int argc, char* argv[]) {
     rclcpp::shutdown();
     cpm_thread.join();
     return 0;
-    /*rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<Listener>());
-    rclcpp::shutdown();
-    return 0;*/
 }
