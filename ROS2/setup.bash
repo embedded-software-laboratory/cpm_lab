9136543c2c0cd4cb061f2f0dbf1d0eae12f04797
janismac <Janis.Maczijewski@rwth-acchen.de>
2018-02-26 13:58:16 +0100

    WIP importing IPS from ROS1

--- ROS2/setup.bash
+++ ROS2/setup.bash
@@ -1,3 +1,4 @@
 source /opt/ros/ardent/setup.bash
 ament build
 source install/setup.bash
+ros2 run cpm_tools cpm_tools_unittest
---
+++ ROS2/src/cpm_tools/src/cpm_tools/unittest.cpp
@@ -0,0 +1,3 @@
+// Generate Catch main() function.
+#define CATCH_CONFIG_MAIN
+#include "cpm_tools/catch.hpp"
