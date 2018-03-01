f0f68a7c23c8955c517e55bd4510868bf6366377
janismac <Janis.Maczijewski@rwth-acchen.de>
2018-03-01 14:41:48 +0100

    refactoring IPS detection node to CpmNode form

--- ROS2/src/ips/src/tools/active_beacon_light_detection/main.cpp
+++ ROS2/src/ips/src/tools/active_beacon_light_detection/main.cpp
@@ -3,9 +3,7 @@
 #include <opencv2/opencv.hpp>
 #include "CameraWrapper/CameraWrapper.hpp"
 #include "CameraParameters/CameraParameters.hpp"
-#include "cpm_tools/AbsoluteTimer.hpp"
-
-bool loop = true;
+#include "cpm_msgs/msg/vehicle_observation.hpp"
 
 struct DetectionVisualizationInfo {
     vector<cv::Point2f> detections;
@@ -13,7 +11,6 @@ struct DetectionVisualizationInfo {
     string camera_serial_number;
     shared_ptr<CameraParameters> cameraParameters;
 };
-
 
 std::array<double, 2> floor_projection_point(std::array<double, 2> point,
     shared_ptr<CameraParameters> cameraParameters) {
@@ -79,104 +76,127 @@ vector<cv::Point2f> detect_light_blobs(cv::Mat img) {
     return detections;
 }
 
-void processFrame(
-    shared_ptr<CameraWrapper> camera,
-    shared_ptr<CameraParameters> params,
-    shared_ptr< ThreadSafeQueue< DetectionVisualizationInfo> > visualization_queue) {
-    
-    camera->triggerExposure();
-    WithTimestamp<cv::Mat> image;
-    if(!camera->grabImage(image)) {
-        loop = false;
-        return;
-    }
-
-    vector<cv::Point2f> detected_light_blobs = detect_light_blobs(image);
-
-    cout << detected_light_blobs.size();
-    cout << endl;
-
-    // Send visualization info
-    {
-        DetectionVisualizationInfo detectionVisualizationInfo;
-        detectionVisualizationInfo.camera_serial_number = camera->getSerialNumber();
-        detectionVisualizationInfo.detections = detected_light_blobs;
-        detectionVisualizationInfo.cameraParameters = params;
-        detectionVisualizationInfo.image = image.clone();
-        visualization_queue->write_nonblocking(detectionVisualizationInfo);
-    } 
-
-}
 
 
-void visualization_loop(shared_ptr< ThreadSafeQueue< DetectionVisualizationInfo > > visualization_queue) {
-    while(loop) {
-        DetectionVisualizationInfo info;
-        if(!visualization_queue->read(info)) return;
+using namespace cpm_tools;
 
-        
-        // visualize
-        if(info.image.rows > 0 && info.image.cols > 0) {
-            cv::cvtColor(info.image, info.image, CV_GRAY2BGR);
-
-            for (auto const &detection: info.detections) {
-                cv::circle(info.image, detection, 8, cv::Scalar(0,0,255));
-            }
-
-            //cv::resize(info.image, info.image, cv::Size(), 0.5, 0.5);
-            cv::imshow(info.camera_serial_number, info.image);
-
-            int key = cv::waitKey(1);
-            if (key == 27) loop = false;
-        }
-    }
-}
-
-
-int main(int argc, char* argv[])
-{
-    vector<string> camera_serial_numbers { "22511669" /* , "21704342" */ };
+class IpsNode : public CpmNode {    
+    rclcpp::Publisher<cpm_msgs::msg::VehicleObservation>::SharedPtr publisher_;
+    vector<string> camera_serial_numbers;
     vector<shared_ptr<CameraWrapper>> cameras;
-
-    for (auto serial_no: camera_serial_numbers) {
-        try {
-            cameras.push_back(make_shared<CameraWrapper>(serial_no));
-        }
-        catch (const std::runtime_error &e) {
-            cout << e.what() << endl;
-            return 1;
-        }
-    }
-
     vector<shared_ptr<CameraParameters>> camera_parameters;
-    for (auto &camera: cameras) {
-        camera->setGainExposure(0,50);
+    shared_ptr<ThreadSafeQueue< DetectionVisualizationInfo > > visualization_queue;
+    thread visualization_thread;
 
-        string extrinsic_parameters_path = "src/ips/cfg/cameras/" + camera->getSerialNumber() + "/extrinsic_parameters.yaml";
-        string intrinsic_parameters_path = "src/ips/cfg/cameras/" + camera->getSerialNumber() + "/intrinsic_parameters.yaml";
+public:
+    IpsNode() : CpmNode("IpsNode", 100 * NANOSEC_PER_MILLISEC, 0, false)
+    {
+        publisher_ = this->create_publisher<cpm_msgs::msg::VehicleObservation>("vehicle04/observation", rmw_qos_profile_sensor_data);
 
-        shared_ptr<CameraParameters> params = make_shared<CameraParameters>();
+        camera_serial_numbers = { "22511669" /* , "21704342" */ };
 
-        params->setExtrinsicParametersFromYAML(extrinsic_parameters_path);
-        params->setIntrinsicParametersFromYAML(intrinsic_parameters_path);
-        camera_parameters.push_back(params);
+        for (auto serial_no: camera_serial_numbers) {
+            try {
+                shared_ptr<CameraWrapper> cam = std::make_shared<CameraWrapper>(serial_no);
+                cameras.push_back(cam);
+            }
+            catch (const std::runtime_error &e) {
+                cout << e.what() << endl;
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        for (auto &camera: cameras) {
+            camera->setGainExposure(0,50);
+
+            string extrinsic_parameters_path = "src/ips/cfg/cameras/" + camera->getSerialNumber() + "/extrinsic_parameters.yaml";
+            string intrinsic_parameters_path = "src/ips/cfg/cameras/" + camera->getSerialNumber() + "/intrinsic_parameters.yaml";
+
+            shared_ptr<CameraParameters> params = std::make_shared<CameraParameters>();
+
+            params->setExtrinsicParametersFromYAML(extrinsic_parameters_path);
+            params->setIntrinsicParametersFromYAML(intrinsic_parameters_path);
+            camera_parameters.push_back(params);
+        }
+
+
+        visualization_queue = std::make_shared<ThreadSafeQueue< DetectionVisualizationInfo >>();
+        visualization_thread = std::thread([this](){this->visualization_loop(this->visualization_queue);});
     }
 
-    auto visualization_queue = make_shared<ThreadSafeQueue< DetectionVisualizationInfo >>();
-
-    
-    vector< shared_ptr< cpm_tools::AbsoluteTimer > > timers;
-
-    for (size_t i = 0; i < cameras.size(); ++i) {
-        auto &camera = cameras[i];
-        auto &camera_parameter = camera_parameters[i];
-        timers.push_back(make_shared<cpm_tools::AbsoluteTimer>(0, 100 * 1000000, 0, 0,[&](){
-            processFrame(camera, camera_parameter, visualization_queue);
-        }));
+    void update(uint64_t deadline_nanoseconds) override 
+    {
+        for (size_t i = 0; i < cameras.size(); ++i) {
+            auto &camera = cameras[i];
+            auto &camera_parameter = camera_parameters[i];
+            processFrame(camera, camera_parameter);
+        }
     }
 
-    visualization_loop(visualization_queue);
+    void processFrame( shared_ptr<CameraWrapper> camera, shared_ptr<CameraParameters> params) {        
+        camera->triggerExposure();
+        WithTimestamp<cv::Mat> image;
+        if(!camera->grabImage(image)) {
+            rclcpp::shutdown();
+            return;
+        }
 
-    for (auto &camera: cameras) camera->close();
-    visualization_queue->close();
+        vector<cv::Point2f> detected_light_blobs = detect_light_blobs(image);
+
+        cout << detected_light_blobs.size();
+        cout << endl;
+
+        // Send visualization info
+        {
+            DetectionVisualizationInfo detectionVisualizationInfo;
+            detectionVisualizationInfo.camera_serial_number = camera->getSerialNumber();
+            detectionVisualizationInfo.detections = detected_light_blobs;
+            detectionVisualizationInfo.cameraParameters = params;
+            detectionVisualizationInfo.image = image.clone();
+            visualization_queue->write_nonblocking(detectionVisualizationInfo);
+        }
+    }
+
+    void visualization_loop(shared_ptr< ThreadSafeQueue< DetectionVisualizationInfo > > visualization_queue) {
+        while(rclcpp::ok()) {
+            DetectionVisualizationInfo info;
+            if(!visualization_queue->read(info)) return;
+
+            
+            // visualize
+            if(info.image.rows > 0 && info.image.cols > 0) {
+                cv::cvtColor(info.image, info.image, CV_GRAY2BGR);
+
+                for (auto const &detection: info.detections) {
+                    cv::circle(info.image, detection, 8, cv::Scalar(0,0,255));
+                }
+
+                //cv::resize(info.image, info.image, cv::Size(), 0.5, 0.5);
+                cv::imshow(info.camera_serial_number, info.image);
+
+                int key = cv::waitKey(1);
+                if (key == 27) {
+                    rclcpp::shutdown();
+                    return;
+                }
+            }
+        }
+    }
+
+    ~IpsNode(){
+        rclcpp::shutdown();
+        for (auto &camera: cameras) camera->close();
+        visualization_queue->close();
+        visualization_thread.join();
+    }
+};
+
+int main(int argc, char* argv[]) {
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<IpsNode>();
+    std::thread node_thread([&](){node->start_loop();});
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    node_thread.join();
+    return 0;
 }
