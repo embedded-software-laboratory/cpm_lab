76b458109bb57db49d3d14616b8697d9ee23ff63
janismac <Janis.Maczijewski@rwth-acchen.de>
2018-04-30 20:06:07 +0200

    blinking on second pairity

--- prototype/sntp_example/sntp_example.c
+++ prototype/sntp_example/sntp_example.c
@@ -24,42 +24,45 @@
 #include <sntp.h>
 #include <time.h>
 
-#define SNTP_SERVERS 	"192.168.0.118", "192.168.0.118", \
-						"192.168.0.118", "3.pool.ntp.org"
+#define SNTP_SERVERS     "192.168.0.118", "192.168.0.118", \
+                        "192.168.0.118", "3.pool.ntp.org"
 
-#define vTaskDelayMs(ms)	vTaskDelay((ms)/portTICK_PERIOD_MS)
-#define UNUSED_ARG(x)	(void)x
+#define vTaskDelayMs(ms)    vTaskDelay((ms)/portTICK_PERIOD_MS)
+#define UNUSED_ARG(x)    (void)x
 
 void sntp_tsk(void *pvParameters)
 {
-	char *servers[] = {SNTP_SERVERS};
-	UNUSED_ARG(pvParameters);
+    char *servers[] = {SNTP_SERVERS};
+    UNUSED_ARG(pvParameters);
 
-	/* Wait until we have joined AP and are assigned an IP */
-	while (sdk_wifi_station_get_connect_status() != STATION_GOT_IP) {
-		vTaskDelayMs(100);
-	}
+    /* Wait until we have joined AP and are assigned an IP */
+    while (sdk_wifi_station_get_connect_status() != STATION_GOT_IP) {
+        vTaskDelayMs(100);
+    }
 
-	/* Start SNTP */
-	printf("Starting SNTP... ");
+    /* Start SNTP */
+    printf("Starting SNTP... ");
     LOCK_TCPIP_CORE();
-	/* SNTP will request an update each 5 minutes */
-	sntp_set_update_delay(15000);
-	/* Set GMT+1 zone, daylight savings off */
-	const struct timezone tz = {1*60, 0};
-	/* SNTP initialization */
-	sntp_initialize(&tz);
-	/* Servers must be configured right after initialization */
-	sntp_set_servers(servers, sizeof(servers) / sizeof(char*));
+    /* SNTP will request an update each 5 minutes */
+    sntp_set_update_delay(15000);
+    /* Set GMT+1 zone, daylight savings off */
+    const struct timezone tz = {1*60, 0};
+    /* SNTP initialization */
+    sntp_initialize(&tz);
+    /* Servers must be configured right after initialization */
+    sntp_set_servers(servers, sizeof(servers) / sizeof(char*));
     UNLOCK_TCPIP_CORE();
-	printf("DONE!\n");
+    printf("DONE!\n");
 
-	/* Print date and time each 5 seconds */
-	while(1) {
-		vTaskDelayMs(5000);
-		time_t ts = time(NULL);
-		printf("TIME: %s", ctime(&ts));
-	}
+    while(1) {
+        int32_t us;
+        time_t ts2 = sntp_get_rtc_time(&us);
+        int32_t secs = ts2;
+        //printf("sec: %i.%06i\n", secs, us);
+
+        gpio_write(5, secs%2);
+        vTaskDelayMs(1);
+    }
 }
 
 void user_init(void)
@@ -71,6 +74,8 @@ void user_init(void)
         .ssid = WIFI_SSID,
         .password = WIFI_PASS,
     };
+    gpio_enable(5, GPIO_OUTPUT);
+    gpio_write(5, 0);
 
     /* required to call wifi_set_opmode before station_set_config */
     sdk_wifi_set_opmode(STATION_MODE);
