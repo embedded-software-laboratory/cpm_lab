6c2d465b192db640dacfd02342eb5711f96fd5a1
janismac <Janis.Maczijewski@rwth-acchen.de>
2018-09-10 19:41:29 +0200

    IMU yaw readout

--- vehicle_esp8266_firmware_v2/i2c_bus.c
+++ vehicle_esp8266_firmware_v2/i2c_bus.c
@@ -11,6 +11,10 @@
 #define BNO055_CHIP_ID      (0xA0)
 #define BNO055_EULER_H_LSB_ADDR (0x1A)
 #define BNO055_EULER_H_MSB_ADDR (0x1B)
+#define BNO055_OPR_MODE_ADDR     0X3D
+#define BNO055_PWR_MODE_ADDR     0X3E
+#define POWER_MODE_NORMAL        0X00
+#define OPERATION_MODE_NDOF      0X0C
 
 
 void writeByte(uint8_t slave_addr, uint8_t register_addr, uint8_t data_byte) {
@@ -31,17 +35,16 @@ uint8_t readByte(uint8_t slave_addr, uint8_t register_addr) {
 void task_i2c_bus(void *pvParameters) {
 
     vTaskDelay(pdMS_TO_TICKS(500));
-    //TickType_t previousWakeTime = xTaskGetTickCount();
-    //vTaskDelayUntil(&previousWakeTime, pdMS_TO_TICKS(10));
 
+    // I2C Bus setup
     if(i2c_init(0, SCL_PIN, SDA_PIN, I2C_FREQ_100K) != 0) {
         printf("Error in i2c_init()\n");
     }
 
     i2c_set_clock_stretch(0, 80000);
-
     vTaskDelay(1);
 
+    // Check for IMU
     uint8_t bno055_chip_id_val = readByte(BNO055_ADDRESS, BNO055_CHIP_ID_ADDR);
     if(bno055_chip_id_val != BNO055_CHIP_ID){
         printf("I2C Error: Expected BNO055 CHIP ID 0xa0, received %x\n", bno055_chip_id_val);
@@ -49,16 +52,28 @@ void task_i2c_bus(void *pvParameters) {
     else {
         printf("I2C Info: BNO055 CHIP found.\n");
     }
+    vTaskDelay(1);
+
+    // Configure IMU
+    writeByte(BNO055_ADDRESS, BNO055_PWR_MODE_ADDR, POWER_MODE_NORMAL);
+    vTaskDelay(1);
+
+    writeByte(BNO055_ADDRESS, BNO055_OPR_MODE_ADDR, OPERATION_MODE_NDOF);
+    vTaskDelay(1);
 
 
     while(1) {
-
-        
         vTaskDelay(pdMS_TO_TICKS(500));
 
-        //uint8_t heading_lsb = readByte(BNO055_ADDRESS, BNO055_CHIP_ID_ADDR);
-        //printf("heading_lsb %8i\n", heading_lsb);
-        
+        uint8_t yaw_data[2];
+        uint8_t yaw_register = BNO055_EULER_H_LSB_ADDR;
+        if(i2c_slave_read(0, BNO055_ADDRESS, &yaw_register, yaw_data, 2)) {
+            printf("Error in i2c_slave_read()\n");
+        }
 
+        uint16_t yaw = ((uint16_t)yaw_data[1])<<8 | ((uint16_t)yaw_data[0]);
+
+
+        printf("yaw %8i\n", yaw);
     }
 }
