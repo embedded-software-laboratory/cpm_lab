f4e25a9da101a93813ad0eca69de7246952476b8
janismac <Janis.Maczijewski@rwth-acchen.de>
2018-09-10 18:37:10 +0200

    WIP IMU driver

--- vehicle_esp8266_firmware_v2/i2c_bus.c
+++ vehicle_esp8266_firmware_v2/i2c_bus.c
@@ -4,9 +4,11 @@
 #include "esp/uart.h"
 
 
-#define SDA_PIN (4)
+#define SDA_PIN (2)
 #define SCL_PIN (0)
 #define BNO055_ADDRESS (0x28)
+#define BNO055_CHIP_ID_ADDR (0x00)
+#define BNO055_CHIP_ID      (0xA0)
 #define BNO055_EULER_H_LSB_ADDR (0x1A)
 #define BNO055_EULER_H_MSB_ADDR (0x1B)
 
@@ -36,6 +38,17 @@ void task_i2c_bus(void *pvParameters) {
         printf("Error in i2c_init()\n");
     }
 
+    i2c_set_clock_stretch(0, 80000);
+
+    vTaskDelay(1);
+
+    uint8_t bno055_chip_id_val = readByte(BNO055_ADDRESS, BNO055_CHIP_ID_ADDR);
+    if(bno055_chip_id_val != BNO055_CHIP_ID){
+        printf("I2C Error: Expected BNO055 CHIP ID 0xa0, received %x\n", bno055_chip_id_val);
+    }
+    else {
+        printf("I2C Info: BNO055 CHIP found.\n");
+    }
 
 
     while(1) {
@@ -43,8 +56,8 @@ void task_i2c_bus(void *pvParameters) {
         
         vTaskDelay(pdMS_TO_TICKS(500));
 
-        uint8_t heading_lsb = readByte(BNO055_ADDRESS, 0);
-        printf("heading_lsb %8i\n", heading_lsb);
+        //uint8_t heading_lsb = readByte(BNO055_ADDRESS, BNO055_CHIP_ID_ADDR);
+        //printf("heading_lsb %8i\n", heading_lsb);
         
 
     }
