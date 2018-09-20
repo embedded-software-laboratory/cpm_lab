1d0128cc7f81ca0772e82d6a3118b5d5ed7fb312
Janis Maczijewski <Janis.Maczijewski@rwth-aachen.de>
2018-09-20 17:00:39 +0200

    corrected comments

--- vehicle_atmega2560_firmware/vehicle_atmega2560_firmware/motor.h
+++ vehicle_atmega2560_firmware/vehicle_atmega2560_firmware/motor.h
@@ -16,7 +16,7 @@
 #define MOTOR_DIRECTION_FORWARD 1
 #define MOTOR_DIRECTION_REVERSE 2
 
-void motor_set_direction(uint8_t direction)
+static inline void motor_set_direction(uint8_t direction)
 {
 	if(direction & 1) {
 		SET_BIT(PORTC, 3);
@@ -31,7 +31,7 @@ void motor_set_direction(uint8_t direction)
 	}
 }
 
-void motor_set_duty(uint16_t duty) // values from 0 to 400
+static inline void motor_set_duty(uint16_t duty) // values from 0 to 400
 {
 	if(duty > 400) {
 		duty = 400;
@@ -39,16 +39,16 @@ void motor_set_duty(uint16_t duty) // values from 0 to 400
 	OCR4B = duty;
 }
 
-void motor_setup()
+static inline void motor_setup()
 {
-	// PWM mode 5: Phase Correct
+	// PWM mode 9: PWM,Phase and Frequency Correct
 	SET_BIT(TCCR4B, WGM43);
 	SET_BIT(TCCR4A, WGM40);
 	
 	// Set PWM frequency to 20kHz
 	OCR4A = 400;
 	
-	// Enable output on Pin 1 / OC0B
+	// Enable output on Pin 16 / OC4B / PH4
 	SET_BIT(TCCR4A, COM4B1);
 	SET_BIT(DDRH, 4);
 	
