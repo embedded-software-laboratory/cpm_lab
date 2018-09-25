905fec11049bb057b609d2e6df05d4baf2a317e1
janismac <Janis.Maczijewski@rwth-acchen.de>
2018-09-25 15:32:03 +0200

    receiving telemetry via WIFI

--- vehicle_esp8266_firmware_v2/main.c
+++ vehicle_esp8266_firmware_v2/main.c
@@ -7,19 +7,20 @@
 #include "esp/timer.h"
 #include "ssid_config.h"
 #include "spi_atmega.h"
-
+#include "wifi_telemetry.h"
 
 
 void task_main(void *pvParameters) {
 
 
-    vTaskDelay(pdMS_TO_TICKS(2000));
+    vTaskDelay(pdMS_TO_TICKS(5000));
     //TickType_t previousWakeTime = xTaskGetTickCount();
     //vTaskDelayUntil(&previousWakeTime, pdMS_TO_TICKS(10));
 
     uint8_t count = 0;
 
     while(1) {
+
 
 
         spi_mosi_data_t send_data;
@@ -33,7 +34,11 @@ void task_main(void *pvParameters) {
 
         spi_miso_data_t recv_data = spi_atmega_exchange(send_data);
 
-        printf("tick %i debugA %i debugC %i speed %i\n", recv_data.tick, send_data.debugA, recv_data.debugC, recv_data.speed);
+        //printf("tick %i debugA %i debugC %i speed %i\n", recv_data.tick, send_data.debugA, recv_data.debugC, recv_data.speed);
+
+        //char msg[] = "Hallo\n";
+
+        wifi_telemetry_send((void*)(&recv_data), sizeof(spi_miso_data_t));
 
         count++;
         vTaskDelay(pdMS_TO_TICKS(50));
@@ -48,15 +53,16 @@ void user_init(void)
 
     /** WiFi config **/
     sdk_wifi_set_opmode(0); // disable
-    //struct sdk_station_config config = {
-    //    .ssid = WIFI_SSID,
-    //    .password = WIFI_PASS,
-    //};
-    //sdk_wifi_set_opmode(STATION_MODE);
-    //sdk_wifi_station_set_config(&config);
+    struct sdk_station_config config = {
+        .ssid = WIFI_SSID,
+        .password = WIFI_PASS,
+    };
+    sdk_wifi_set_opmode(STATION_MODE);
+    sdk_wifi_station_set_config(&config);
 
     /** Init modules **/
     spi_atmega_setup();
+    wifi_telemetry_setup();
 
     /** Start tasks **/
     xTaskCreate(task_main, "task_main", 512, NULL, 2, NULL);
