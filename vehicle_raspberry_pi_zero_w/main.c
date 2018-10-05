2957583c2e98ddf138bed4b53511c64173ecfdb8
janismac <Janis.Maczijewski@rwth-acchen.de>
2018-10-05 19:23:27 +0200

    remote control test via UDP, RPi

--- vehicle_raspberry_pi_zero_w/main.c
+++ vehicle_raspberry_pi_zero_w/main.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include "spi.h"
+#include "udp_tmp.h"
 
 int main(int argc, char **argv) {
 
@@ -15,15 +16,34 @@ int main(int argc, char **argv) {
     spi_init();
 
 
+    int sock = make_socket();
+
+    ip_addr target = make_addr("192.168.1.119", 6780);
+
+    int counter = 0;
+
     while (1) {
         spi_mosi_data_t spi_mosi_data = {0};
-        spi_miso_data_t spi_miso_data = spi_transfer(spi_mosi_data);
+        char sender_ip[100];
 
-        printf("tick %u\n", spi_miso_data.tick);
-        printf("odometer_steps %i\n", spi_miso_data.odometer_steps);
-        printf("imu_yaw %u\n", spi_miso_data.imu_yaw);
-        printf("\n");
-        usleep(60000);
+        int recv_len = 1;
+
+        while(recv_len > 0) {
+            recv_len = receive(sock, sender_ip, (void*)&spi_mosi_data, sizeof(spi_mosi_data_t));
+        }
+
+
+        spi_miso_data_t spi_miso_data = spi_transfer(spi_mosi_data);
+        counter++;
+        /*if(counter % 20 == 0) {
+            printf("tick %u\n", spi_miso_data.tick);
+            printf("odometer_steps %i\n", spi_miso_data.odometer_steps);
+            printf("imu_yaw %u\n", spi_miso_data.imu_yaw);
+            printf("\n");
+        }*/
+
+        send_to(sock, target, &spi_miso_data, sizeof(spi_miso_data_t));
+        usleep(20000);
     }
 
     return 0;
