2498a4625f84483259a0623f6e0d0aaa7e31bda2
janismac <Janis.Maczijewski@rwth-acchen.de>
2018-10-12 18:38:54 +0200

    dead reckoning on RPi

--- rti_raspberry/src/main.cxx
+++ rti_raspberry/src/main.cxx
@@ -2,6 +2,8 @@
 #include <cstring>
 #include <vector>
 #include <iterator>
+#include <iomanip>
+#include <ios>
 using std::vector;
 
 #include <dds/pub/ddspub.hpp>
@@ -18,6 +20,14 @@ extern "C" {
 #include "spi.h"
 #include "../../vehicle_atmega2560_firmware/vehicle_atmega2560_firmware/crc.c"
 }
+
+
+uint64_t clock_gettime_nanoseconds() {
+    struct timespec t;
+    clock_gettime(CLOCK_REALTIME, &t);
+    return uint64_t(t.tv_sec) * 1000000000ull + uint64_t(t.tv_nsec);
+}
+
 
 int main(/*int argc, char *argv[]*/)
 {
@@ -49,6 +59,9 @@ int main(/*int argc, char *argv[]*/)
 
     VehicleCommand latest_command;
     int latest_command_TTL = 0;
+    double position_x = 0;
+    double position_y = 0;
+    double odometer_meters_prev = 0;
 
 
     AbsoluteTimer timer_loop(0, 20000000, 0, 0, [&](){
@@ -105,22 +118,35 @@ int main(/*int argc, char *argv[]*/)
 
         const double odometer_meter_per_step = 0.00468384074941;
         const double speed_meter_per_second_per_step = odometer_meter_per_step * 0.2384185791;
-        const double imu_yaw_radian_per_step = 0.00109083078249645598;
+        const double imu_yaw_radian_per_step = -0.00109083078249645598;
         const double battery_volt_per_step = 0.0116669;
 
         if(miso_CRC_actual == mosi_CRC_target) {
+            const double yaw_radian = spi_miso_data.imu_yaw * imu_yaw_radian_per_step;
+            const double odometer_meters = spi_miso_data.odometer_steps * odometer_meter_per_step;
+            const double ds = odometer_meters - odometer_meters_prev;
+            odometer_meters_prev = odometer_meters;
+
+            if(-0.5 < ds && ds < 0.5) {
+                position_x += ds * cos(yaw_radian);
+                position_y += ds * sin(yaw_radian);
+            }
+
+
             VehicleState sample;
-            sample.odometer_distance           (spi_miso_data.odometer_steps * odometer_meter_per_step);
-            sample.imu_yaw                     (spi_miso_data.imu_yaw * imu_yaw_radian_per_step);
+            sample.odometer_distance           (odometer_meters);
+            sample.pose().x                    (position_x);
+            sample.pose().y                    (position_y);
+            sample.pose().yaw                  (yaw_radian);
             sample.imu_acceleration_forward    (spi_miso_data.imu_acceleration_forward * 0.01);
             sample.imu_acceleration_left       (spi_miso_data.imu_acceleration_left * 0.01);
             sample.speed                       (spi_miso_data.speed * speed_meter_per_second_per_step);
             sample.battery_voltage             (spi_miso_data.battery_voltage * battery_volt_per_step);
-            sample.motor_current               (spi_miso_data.motor_current);
+            sample.motor_current               (spi_miso_data.motor_current); // TODO calibrate current
             writer_vehicleState.write(sample);
         }
         else {
-            std::cerr << "Data corruption on ATmega SPI bus. CRC mismatch." << std::endl;
+            std::cerr << "[" << std::fixed << std::setprecision(9) << double(clock_gettime_nanoseconds())/1e9 <<  "] Data corruption on ATmega SPI bus. CRC mismatch." << std::endl;
         }
 
 
