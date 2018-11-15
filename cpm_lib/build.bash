589d33c0261d1b3d42658f53ff3833989140144e
Janis Maczijewski <janismac@gmx.de>
2018-11-15 23:12:57 +0100

    IDL for parameter service

--- cpm_lib/build.bash
+++ cpm_lib/build.bash
@@ -7,6 +7,12 @@ if [ ! -d "build" ]; then
     mkdir build
 fi
 
+if [ ! -d "build/rti" ]; then
+    mkdir build/rti
+    rtiddsgen -language C++11 -d ./build/rti/ ../dds_idl/Parameter.idl
+fi
+
+
 cd build
 cmake .. 
 make -j8
