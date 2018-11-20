856fc62fb7e5aea67544e7f64ace85e8e2f79be1
janismac <Janis.Maczijewski@rwth-acchen.de>
2018-11-20 11:04:34 +0100

    cleanup

--- vehicle_raspberry_pi_zero_w/build.bash
+++
@@ -1,7 +0,0 @@
-export RPI_TOOL_PATH=$(pwd)/cross_compile/tools
-export MYPWD=$(pwd)
-mkdir build
-cd build
-cmake .. -DCMAKE_TOOLCHAIN_FILE=$MYPWD/cross_compile/Toolchain-rpi.cmake
-make
-cd ..
--- vehicle_raspberry_pi_zero_w/spi.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "../vehicle_atmega2560_firmware/vehicle_atmega2560_firmware/spi_packets.h"
-
-void spi_init();
-spi_miso_data_t spi_transfer(spi_mosi_data_t spi_mosi_data);
