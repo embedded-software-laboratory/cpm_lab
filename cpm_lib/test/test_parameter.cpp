72b985aa39dbd967ef2320363feab543d9a1a619
janismac <Janis.Maczijewski@rwth-acchen.de>
2018-11-28 12:00:51 +0100

    fixed parameter test case

--- cpm_lib/test/test_parameter.cpp
+++ cpm_lib/test/test_parameter.cpp
@@ -6,23 +6,16 @@
 #include <chrono>
 
 TEST_CASE( "parameter" ) {
-    
-    // TODO create instance of parameter server,
-    //      distribute parameter "my_param_name" = (int) 42
+
+    double received_parameter_value = 0;
+    std::thread client_thread([&](){
+        received_parameter_value = cpm::parameter_double("my_param_name");
+    });
+
     ParameterServer server(0, "parameterRequest", "parameter");
-    server.set_value("my_param_name", 42);
+    server.set_value("my_param_name", 42.1);
 
+    client_thread.join();
 
-    std::shared_ptr<int32_t> received_parameter_value = nullptr;
-
-    int32_t value = cpm::parameter_int("my_param_name");
-    received_parameter_value = std::make_shared<int32_t>(value);
-
-    // TODO kill parameter_client_thread and parameter_server_thread. 
-    // This seems impossible with c++11.
-    // See https://stackoverflow.com/questions/12207684/how-do-i-terminate-a-thread-in-c11
-    // Maybe change the design, add a timeout?
-
-    REQUIRE( bool(received_parameter_value) );
-    REQUIRE( *received_parameter_value == 42 );
+    REQUIRE( received_parameter_value == 42.1 );
 }
