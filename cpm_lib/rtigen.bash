d5334044888bf256c3c114930f6296b5222f82b1
janismac <Janis.Maczijewski@rwth-acchen.de>
2018-11-30 16:31:24 +0100

    build automation for IDL files

--- cpm_lib/rtigen.bash
+++ cpm_lib/rtigen.bash
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-if [ ! -d "rti" ]; then
-    mkdir rti
-    rtiddsgen -language C++11 -d ./rti/ ../dds_idl/Parameter.idl
-    rtiddsgen -language C++11 -d ./rti/ ../dds_idl/ParameterRequest.idl
+if [ ! -d "dds" ]; then
+    mkdir dds
+
+    # Generate IDL C++
+    find ../dds_idl/ -type f | xargs -n 1 rtiddsgen -language C++11 -d ./dds/
+fi
+
+if [ ! -d "include/cpm/dds" ]; then
+    mkdir include/cpm/dds
+
+    # Copy headers to public inclues
+    (cd ../dds_idl/; find . -type f) | sed -r 's/..(.*).idl/dds\/\1.hpp/' | xargs -n 1 -I ARG cp ARG include/cpm/dds/
 fi
