d03dee48a028177aa0f9860b9512ac785ffbd72d
janismac <Janis.Maczijewski@rwth-acchen.de>
2018-12-05 15:37:33 +0100

    WIP: moving LabControlCenter to cpm_lib

--- LabControlCenter/build.bash
+++ LabControlCenter/build.bash
@@ -1,18 +1,8 @@
 #!/bin/bash
 
 clear
-rm -rf build
-mkdir build
-rm -rf rtidds
-mkdir rtidds
 
-rtiddsgen -language C++11 -d ./rtidds/ ../dds_idl/VehicleState.idl
-rtiddsgen -language C++11 -d ./rtidds/ ../dds_idl/TimeStamp.idl
-rtiddsgen -language C++11 -d ./rtidds/ ../dds_idl/Header.idl
-rtiddsgen -language C++11 -d ./rtidds/ ../dds_idl/VehicleCommandDirect.idl
-rtiddsgen -language C++11 -d ./rtidds/ ../dds_idl/Pose2D.idl
-rtiddsgen -language C++11 -d ./rtidds/ ../dds_idl/VehicleCommandTrajectory.idl
-rtiddsgen -language C++11 -d ./rtidds/ ../dds_idl/VehicleCommandSpeedCurvature.idl
+mkdir build
 
 cd build
 cmake .. 
