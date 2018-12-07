8162d581335047956e67fb6ee602042b350dadff
janismac <Janis.Maczijewski@rwth-acchen.de>
2018-12-07 13:54:22 +0100

    DDS Reader does not behave as expected. Implemented own sample ring buffer

--- cpm_lib/include/cpm/Reader.hpp
+++ cpm_lib/include/cpm/Reader.hpp
@@ -4,12 +4,30 @@
 
 #include "cpm/ParticipantSingleton.hpp"
 
+#define CPM_READER_RING_BUFFER_SIZE (32)
+
 namespace cpm
 {
     template<typename T>
     class Reader
     {
         dds::sub::DataReader<T> dds_reader;
+
+        T ring_buffer[CPM_READER_RING_BUFFER_SIZE];
+        size_t ring_buffer_index = 0; // index of next write == index of oldest element
+
+        void flush_dds_reader()
+        {
+            auto samples = dds_reader.take();
+            for(auto sample: samples)
+            {
+                if(sample.info().valid()) 
+                {
+                    ring_buffer[ring_buffer_index] = sample.data();
+                    ring_buffer_index = (ring_buffer_index+1) % CPM_READER_RING_BUFFER_SIZE;
+                }
+            }
+        }
 
     public:
         Reader(dds::topic::Topic<T> &topic)
@@ -27,50 +45,30 @@ namespace cpm
 
         void get_sample(const uint64_t t_now, T& sample_out, uint64_t& sample_age_out)
         {
+            flush_dds_reader();
+
             sample_out = T();
             sample_out.header().create_stamp().nanoseconds(0);
             sample_age_out = t_now;
 
-
             // select sample
-
-            // TODO would this be more efficient with a dds::sub::cond::QueryCondition?
-
-            auto samples = dds_reader.read();
-            for(auto sample: samples)
+            for (int i = 0; i < CPM_READER_RING_BUFFER_SIZE; ++i)
             {
-                if(!sample.info().valid()) 
-                {
-                    continue;
-                }
+                auto& current_sample = ring_buffer[(i + ring_buffer_index) % CPM_READER_RING_BUFFER_SIZE];
 
-                auto sample_data = sample.data();
-
-                if(sample_data.header().valid_after_stamp().nanoseconds() > t_now) 
+                if(current_sample.header().valid_after_stamp().nanoseconds() > t_now) 
                 {
                     // Data is "in the future", ignore for now
                     continue;
                 }
 
-                if(sample_out.header().create_stamp().nanoseconds() <= sample_data.header().create_stamp().nanoseconds())
+                if(sample_out.header().create_stamp().nanoseconds() <= current_sample.header().create_stamp().nanoseconds())
                 {
                     // Current sample has a higher timestamp, it is newer. Use it.
-                    sample_out = sample_data;
+                    sample_out = current_sample;
                     sample_age_out = t_now - sample_out.header().valid_after_stamp().nanoseconds();
                 }
             }
-
-
-            // discard old samples (older than 10 sec)
-            if(t_now > 10000000000ull) // prevent unsigned underflow
-            {
-                const uint64_t t_discard = t_now - 10000000000ull;
-                auto discarded_samples = dds_reader
-                    .select()
-                    .content(dds::sub::Query(dds_reader, "header.valid_after_stamp.nanoseconds < " 
-                        + std::to_string(t_discard)))
-                    .take();
-            }            
         }
     };
 
