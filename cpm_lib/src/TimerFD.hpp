0b62554adbb282f8bc9e05c8f65f6fe09b912fa5
janismac <Janis.Maczijewski@rwth-acchen.de>
2018-12-07 12:57:06 +0100

    timer bugfix

--- cpm_lib/src/TimerFD.hpp
+++ cpm_lib/src/TimerFD.hpp
@@ -10,11 +10,13 @@ class TimerFD : public cpm::Timer
     uint64_t period_nanoseconds; 
     uint64_t offset_nanoseconds;
     std::thread runner_thread;
+    std::function<void(uint64_t t_now)> m_update_callback;
 
     void wait();
 
 public:
     TimerFD(uint64_t period_nanoseconds, uint64_t offset_nanoseconds);
+    ~TimerFD();
 
     void start       (std::function<void(uint64_t t_now)> update_callback) override;
     void start_async (std::function<void(uint64_t t_now)> update_callback) override;
