ee35911a57d60ffb872dae6555ac2a825b832037
Janis Maczijewski <Janis.Maczijewski@rwth-aachen.de>
2019-01-08 18:10:37 +0100

    cleanup ROS2

--- ROS2/setup.bash
+++
@@ -1,5 +0,0 @@
-source /opt/ros/ardent/setup.bash
-ament build
-source install/setup.bash
-ros2 run cpm_tools cpm_tools_unittest
-ros2 run ips ips_unittest
--- ROS2/src/ips/src/tools/unittest/main.cpp
+++
@@ -1,3 +0,0 @@
-// Generate Catch main() function.
-#define CATCH_CONFIG_MAIN
-#include "cpm_tools/catch.hpp"
