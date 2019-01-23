75488dbc58d38610b41560eb9c6ac3678e010c65
Janis Maczijewski <Janis.Maczijewski@rwth-aachen.de>
2019-01-23 14:49:59 +0100

    tools\camera_calibration\calibrationCheckerboardPoints.m

---
+++ tools/camera_calibration/calibrationCheckerboardPoints.m
@@ -0,0 +1,35 @@
+function calibrationCheckerboardPoints
+
+    if exist('checkerboardPoints.mat','file')
+        load checkerboardPoints
+    else
+        I=imread('C:\Users\janis\sciebo\CPM\Project\Lab\07_Students\18_MA_Tuelleners\06_Measurement\calibration.png');
+        [imagePoints,boardSize] = detectCheckerboardPoints(I);
+        save checkerboardPoints
+    end
+    
+    imagePoints = reshape(imagePoints, boardSize(1)-1, boardSize(2)-1, 2);
+    
+    imagePoints = flipud(imagePoints);
+    
+    f = fopen('calibration_points.csv', 'w');
+    fprintf(f,'world_x; world_y; image_x; image_y;\n');
+    
+    for ix = 1:size(imagePoints, 2)
+        for iy = 1:size(imagePoints, 1)
+            row = [(ix/10);(iy/10); squeeze(imagePoints(iy,ix,:))];
+            fprintf(f,'%3.4f; ', row);
+            fprintf(f,'\n');
+        end
+    end
+    
+    
+    fclose(f);
+
+    clf
+    imshow(I);
+    hold on;
+    plot(imagePoints(1:10,1,1),imagePoints(1:10,1,2),'ro');
+
+end
+
