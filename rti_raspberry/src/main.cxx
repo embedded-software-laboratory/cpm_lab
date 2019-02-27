2638cbcefaf941dd6f217cfe57e24c66195579d0
Isabelle Tülleners <isabelle.tuelleners@rwth-aachen.de>
2019-02-27 19:32:38 +0100

    Adapt enable ticks to match vehicle ids

--- rti_raspberry/src/main.cxx
+++ rti_raspberry/src/main.cxx
@@ -108,8 +108,8 @@ int main(int argc, char *argv[])
 
     crcInit();
 
-    const vector<uint8_t> identification_LED_period_ticks  {4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4};
-    const vector<uint8_t> identification_LED_enabled_ticks {2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2};
+	const vector<uint8_t> identification_LED_period_ticks  { 1, 4, 7, 10, 13, 16, 7, 10, 13, 16, 19, 10, 13, 16, 19, 22, 13, 16, 19, 22, 25, 16, 19, 22, 25, 28 };
+    const vector<uint8_t> identification_LED_enabled_ticks { 0, 2, 2,  2,  2,  2, 5,  5,  5,  5,  5,  8,  8,  8,  8,  8, 11, 11, 11, 11, 11, 14, 14, 14, 14, 14 };
     
     // Loop setup
     Localization localization;
