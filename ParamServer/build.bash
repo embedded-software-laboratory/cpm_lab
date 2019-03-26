2cd5eafce41fff2e1618580af0975cb60406f4de
leon.rabanus <leon.rabanus@rwth-aachen.de>
2019-03-26 12:46:01 +0100

    Changed yaml integration
    Former-commit-id: 64a4d63dbaecc676ed377d61739023ec3f6375e4

--- ParamServer/build.bash
+++ ParamServer/build.bash
@@ -1,7 +1,17 @@
 #!/bin/bash
-
 clear
 
+# Get yaml
+git clone https://github.com/jbeder/yaml-cpp.git
+cd yaml-cpp
+mkdir build
+cd build
+cmake .. -DBUILD_SHARED_LIBS=ON
+make
+
+# Make the rest
+cd ..
+cd ..
 mkdir build
 
 cd build
