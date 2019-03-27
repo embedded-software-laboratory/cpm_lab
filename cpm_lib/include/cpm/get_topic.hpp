df912417916679b0935a244eb926eab280f09627
janismac <Janis.Maczijewski@rwth-acchen.de>
2019-03-27 17:14:08 +0100

    WIP get_topic implementation

---
+++ cpm_lib/include/cpm/get_topic.hpp
@@ -0,0 +1,20 @@
+#include <dds/topic/Topic.hpp>
+#include <dds/topic/ddstopic.hpp>
+#include <string>
+#include "cpm/ParticipantSingleton.hpp"
+
+namespace cpm
+{
+    template<typename T>
+    dds::topic::Topic<T> get_topic(const dds::domain::DomainParticipant& participant, std::string topic_name)
+    {
+        return dds::topic::find<dds::topic::Topic<T>>(participant, topic_name);
+    }
+
+
+    template<typename T>
+    dds::topic::Topic<T> get_topic(std::string topic_name)
+    {
+        return get_topic<T>(cpm::ParticipantSingleton::Instance(), topic_name);
+    }
+}
