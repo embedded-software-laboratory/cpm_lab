7a7054941656e089f83031e1bc978c65b2567906
janismac <Janis.Maczijewski@rwth-acchen.de>
2019-03-27 18:56:00 +0100

    receiving IPS data in LCC

--- LabControlCenter/src/TimeSeriesAggregator.cpp
+++ LabControlCenter/src/TimeSeriesAggregator.cpp
@@ -11,41 +11,59 @@ TimeSeriesAggregator::TimeSeriesAggregator()
         cpm::ParticipantSingleton::Instance(),
         cpm::get_topic<VehicleState>("vehicleState")
     );
+
+
+    vehicle_observation_reader = make_shared<cpm::AsyncReader<VehicleObservation>>(
+        [this](dds::sub::LoanedSamples<VehicleObservation>& samples){
+            handle_new_vehicleObservation_samples(samples);
+        },
+        cpm::ParticipantSingleton::Instance(),
+        cpm::get_topic<VehicleObservation>("vehicleObservation")
+    );
 }
 
 
 void TimeSeriesAggregator::create_vehicle_timeseries(uint8_t vehicle_id) 
 {
-    timeseries_vehicleState[vehicle_id] = map<string, shared_ptr<TimeSeries>>();
+    timeseries_vehicles[vehicle_id] = map<string, shared_ptr<TimeSeries>>();
 
-    timeseries_vehicleState[vehicle_id]["pose_x"] = make_shared<TimeSeries>(
+    timeseries_vehicles[vehicle_id]["pose_x"] = make_shared<TimeSeries>(
         "Position X", "%6.2f", "m");
 
-    timeseries_vehicleState[vehicle_id]["pose_y"] = make_shared<TimeSeries>(
+    timeseries_vehicles[vehicle_id]["pose_y"] = make_shared<TimeSeries>(
         "Position Y", "%6.2f", "m");
 
-    timeseries_vehicleState[vehicle_id]["pose_yaw"] = make_shared<TimeSeries>(
+    timeseries_vehicles[vehicle_id]["pose_yaw"] = make_shared<TimeSeries>(
         "Yaw", "%6.3f", "rad");
 
-    timeseries_vehicleState[vehicle_id]["odometer_distance"] = make_shared<TimeSeries>(
+    timeseries_vehicles[vehicle_id]["ips_x"] = make_shared<TimeSeries>(
+        "IPS Position X", "%6.2f", "m");
+
+    timeseries_vehicles[vehicle_id]["ips_y"] = make_shared<TimeSeries>(
+        "IPS Position Y", "%6.2f", "m");
+
+    timeseries_vehicles[vehicle_id]["ips_yaw"] = make_shared<TimeSeries>(
+        "IPS Yaw", "%6.3f", "rad");
+
+    timeseries_vehicles[vehicle_id]["odometer_distance"] = make_shared<TimeSeries>(
         "Odometer Distance", "%7.2f", "m");
 
-    timeseries_vehicleState[vehicle_id]["imu_acceleration_forward"] = make_shared<TimeSeries>(
+    timeseries_vehicles[vehicle_id]["imu_acceleration_forward"] = make_shared<TimeSeries>(
         "Acceleration Forward", "%4.1f", "m/s^2");
 
-    timeseries_vehicleState[vehicle_id]["imu_acceleration_left"] = make_shared<TimeSeries>(
+    timeseries_vehicles[vehicle_id]["imu_acceleration_left"] = make_shared<TimeSeries>(
         "Acceleration Left", "%4.1f", "m/s^2");
 
-    timeseries_vehicleState[vehicle_id]["speed"] = make_shared<TimeSeries>(
+    timeseries_vehicles[vehicle_id]["speed"] = make_shared<TimeSeries>(
         "Speed", "%5.2f", "m/s");
 
-    timeseries_vehicleState[vehicle_id]["battery_voltage"] = make_shared<TimeSeries>(
+    timeseries_vehicles[vehicle_id]["battery_voltage"] = make_shared<TimeSeries>(
         "Battery Voltage", "%5.2f", "V");
 
-    timeseries_vehicleState[vehicle_id]["motor_current"] = make_shared<TimeSeries>(
+    timeseries_vehicles[vehicle_id]["motor_current"] = make_shared<TimeSeries>(
         "Motor Current", "%5.2f", "A");
 
-    timeseries_vehicleState[vehicle_id]["clock_delta"] = make_shared<TimeSeries>(
+    timeseries_vehicles[vehicle_id]["clock_delta"] = make_shared<TimeSeries>(
         "Clock Delta", "%5.1f", "ms");
 }
 
@@ -58,22 +76,44 @@ void TimeSeriesAggregator::handle_new_vehicleState_samples(dds::sub::LoanedSampl
         if(sample.info().valid())
         {
             VehicleState state = sample.data();
-            if(timeseries_vehicleState.count(state.vehicle_id()) == 0)
+            if(timeseries_vehicles.count(state.vehicle_id()) == 0)
             {
                 create_vehicle_timeseries(state.vehicle_id());
             }
 
-            timeseries_vehicleState[state.vehicle_id()]["pose_x"]                   ->push_sample(now, state.pose().x());
-            timeseries_vehicleState[state.vehicle_id()]["pose_y"]                   ->push_sample(now, state.pose().y());
-            timeseries_vehicleState[state.vehicle_id()]["pose_yaw"]                 ->push_sample(now, state.pose().yaw());
-            timeseries_vehicleState[state.vehicle_id()]["odometer_distance"]        ->push_sample(now, state.odometer_distance());
-            timeseries_vehicleState[state.vehicle_id()]["imu_acceleration_forward"] ->push_sample(now, state.imu_acceleration_forward());
-            timeseries_vehicleState[state.vehicle_id()]["imu_acceleration_left"]    ->push_sample(now, state.imu_acceleration_left());
-            timeseries_vehicleState[state.vehicle_id()]["speed"]                    ->push_sample(now, state.speed());
-            timeseries_vehicleState[state.vehicle_id()]["battery_voltage"]          ->push_sample(now, state.battery_voltage());
-            timeseries_vehicleState[state.vehicle_id()]["motor_current"]            ->push_sample(now, state.motor_current());
+            timeseries_vehicles[state.vehicle_id()]["pose_x"]                   ->push_sample(now, state.pose().x());
+            timeseries_vehicles[state.vehicle_id()]["pose_y"]                   ->push_sample(now, state.pose().y());
+            timeseries_vehicles[state.vehicle_id()]["pose_yaw"]                 ->push_sample(now, state.pose().yaw());
+            timeseries_vehicles[state.vehicle_id()]["odometer_distance"]        ->push_sample(now, state.odometer_distance());
+            timeseries_vehicles[state.vehicle_id()]["imu_acceleration_forward"] ->push_sample(now, state.imu_acceleration_forward());
+            timeseries_vehicles[state.vehicle_id()]["imu_acceleration_left"]    ->push_sample(now, state.imu_acceleration_left());
+            timeseries_vehicles[state.vehicle_id()]["speed"]                    ->push_sample(now, state.speed());
+            timeseries_vehicles[state.vehicle_id()]["battery_voltage"]          ->push_sample(now, state.battery_voltage());
+            timeseries_vehicles[state.vehicle_id()]["motor_current"]            ->push_sample(now, state.motor_current());
 
-            timeseries_vehicleState[state.vehicle_id()]["clock_delta"]              ->push_sample(now, double(now - state.header().create_stamp().nanoseconds())/1e6 );
+            timeseries_vehicles[state.vehicle_id()]["clock_delta"]              ->push_sample(now, double(now - state.header().create_stamp().nanoseconds())/1e6 );
+
+        }
+    }
+}
+
+
+void TimeSeriesAggregator::handle_new_vehicleObservation_samples(dds::sub::LoanedSamples<VehicleObservation>& samples)
+{
+    const uint64_t now = clock_gettime_nanoseconds();
+    for(auto sample : samples)
+    {
+        if(sample.info().valid())
+        {
+            VehicleObservation state = sample.data();
+            if(timeseries_vehicles.count(state.vehicle_id()) == 0)
+            {
+                create_vehicle_timeseries(state.vehicle_id());
+            }
+
+            timeseries_vehicles[state.vehicle_id()]["ips_x"]  ->push_sample(now, state.pose().x());
+            timeseries_vehicles[state.vehicle_id()]["ips_y"]  ->push_sample(now, state.pose().y());
+            timeseries_vehicles[state.vehicle_id()]["ips_yaw"]->push_sample(now, state.pose().yaw());
 
         }
     }
--- LabControlCenter/src/TimeSeriesAggregator.hpp
+++ LabControlCenter/src/TimeSeriesAggregator.hpp
@@ -2,18 +2,21 @@
 #include "defaults.hpp"
 #include <dds/sub/ddssub.hpp>
 #include "VehicleState.hpp"
+#include "VehicleObservation.hpp"
 #include "TimeSeries.hpp"
 #include "cpm/AsyncReader.hpp"
 
 class TimeSeriesAggregator
 {
-    map<uint8_t, map<string, shared_ptr<TimeSeries> > > timeseries_vehicleState;
+    map<uint8_t, map<string, shared_ptr<TimeSeries> > > timeseries_vehicles;
     void create_vehicle_timeseries(uint8_t vehicle_id);
     void handle_new_vehicleState_samples(dds::sub::LoanedSamples<VehicleState>& samples);
+    void handle_new_vehicleObservation_samples(dds::sub::LoanedSamples<VehicleObservation>& samples);
 
     shared_ptr<cpm::AsyncReader<VehicleState>> vehicle_state_reader;
+    shared_ptr<cpm::AsyncReader<VehicleObservation>> vehicle_observation_reader;
 
 public:
     TimeSeriesAggregator();
-    const map<uint8_t, map<string, shared_ptr<TimeSeries> > >& get_vehicle_data() { return timeseries_vehicleState; }
+    const map<uint8_t, map<string, shared_ptr<TimeSeries> > >& get_vehicle_data() { return timeseries_vehicles; }
 };
