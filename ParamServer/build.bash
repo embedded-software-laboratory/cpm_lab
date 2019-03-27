3e30c3bc849c5960435323109ed8a3f73c1a9ddb
janismac <Janis.Maczijewski@rwth-acchen.de>
2019-03-27 14:46:33 +0100

    yaml dependency fix

--- ParamServer/build.bash
+++ ParamServer/build.bash
@@ -2,18 +2,19 @@
 clear
 
 # Get yaml
-git clone https://github.com/jbeder/yaml-cpp.git
-cd yaml-cpp
-mkdir build
-cd build
-cmake .. -DBUILD_SHARED_LIBS=ON
-make
+pushd ../..
+if [ ! -d "yaml-cpp" ]; then
+    git clone https://github.com/jbeder/yaml-cpp.git
+    cd yaml-cpp
+    mkdir build
+    cd build
+    cmake .. -DBUILD_SHARED_LIBS=ON
+    make -j8
+fi
+popd
 
 # Make the rest
-cd ..
-cd ..
 mkdir build
-
 cd build
 cmake .. 
 make -j8 && ./unittest
