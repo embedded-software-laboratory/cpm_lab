1b80dc82ceccc11b4f0c8bf5c9689de45f6fbb48
leon.rabanus <leon.rabanus@rwth-aachen.de>
2019-04-05 10:40:01 +0200

    mutex

--- cpm_lib/include/cpm/get_topic.hpp
+++ cpm_lib/include/cpm/get_topic.hpp
@@ -3,6 +3,7 @@
 #include <dds/topic/Topic.hpp>
 #include <dds/topic/ddstopic.hpp>
 #include <string>
+#include <mutex>
 #include "cpm/ParticipantSingleton.hpp"
 
 namespace cpm
@@ -10,6 +11,9 @@ namespace cpm
     template<typename T>
     dds::topic::Topic<T> get_topic(const dds::domain::DomainParticipant& participant, std::string topic_name)
     {
+        static std::mutex get_topic_mutex;
+        std::lock_guard<std::mutex> lock(get_topic_mutex);
+
         try
         {
             return dds::topic::Topic<T>(participant, topic_name);
