10c0b52e7c708b48dd6c94c806ba3732019a4ea7
janismac <Janis.Maczijewski@rwth-acchen.de>
2019-04-12 16:35:59 +0200

    WIP refactoring map view

--- LabControlCenter/ui/map_view/MapViewUi.cpp
+++ LabControlCenter/ui/map_view/MapViewUi.cpp
@@ -2,6 +2,58 @@
 #include <cassert>
 #include <glibmm/main.h>
 
+using DrawingContext = ::Cairo::RefPtr< ::Cairo::Context >;
+
+
+void draw_grid(const DrawingContext& ctx)
+{
+    ctx->save();
+    {
+        ctx->scale(.1, .1);
+        for (int i = 0; i <= 45; ++i)
+        {
+            if(i == 0) {
+                ctx->set_line_width(0.15);
+            }
+            else if(i % 10 == 0) {
+                ctx->set_line_width(0.05);
+            }
+            else {
+                ctx->set_line_width(0.01);
+            }
+
+            ctx->move_to(i,0);
+            ctx->line_to(i,40);
+            ctx->stroke();
+
+            if(i <= 40)
+            {
+                ctx->move_to(0,i);
+                ctx->line_to(45,i);
+                ctx->stroke();
+            }
+        }    
+    }
+    ctx->restore();
+}
+
+void draw_vehicle_past_trajectory(const DrawingContext& ctx, const map<string, shared_ptr<TimeSeries>>& vehicle_timeseries)
+{
+    // Draw vehicle trajectory
+    {
+        vector<double> trajectory_x = vehicle_timeseries.at("pose_x")->get_last_n_values(100);
+        vector<double> trajectory_y = vehicle_timeseries.at("pose_y")->get_last_n_values(100);
+        for (size_t i = 1; i < trajectory_x.size(); ++i)
+        {
+            if(i == 1) ctx->move_to(trajectory_x[i], trajectory_y[i]);
+            else ctx->line_to(trajectory_x[i], trajectory_y[i]);
+        }
+        ctx->set_source_rgb(1,0,0);
+        ctx->set_line_width(0.01);
+        ctx->stroke();
+    }
+}
+
 MapViewUi::MapViewUi(std::function<VehicleData()> get_vehicle_data_callback)
 {
     this->get_vehicle_data = get_vehicle_data_callback;
@@ -47,80 +99,40 @@ MapViewUi::MapViewUi(std::function<VehicleData()> get_vehicle_data_callback)
     });
 
 
-    drawingArea->signal_draw().connect([&](const ::Cairo::RefPtr< ::Cairo::Context >& ctx)->bool {
+    drawingArea->signal_draw().connect([&](const DrawingContext& ctx)->bool {
 
         ctx->save();
         {
             ctx->translate(pan_x, pan_y);
-            ctx->scale(zoom, zoom);
+            ctx->scale(zoom, -zoom);
 
 
-            // Draw grid
-            ctx->save();
-            {
-                ctx->scale(.1, -.1);
-                for (int i = 0; i <= 45; ++i)
-                {
-                    if(i == 0) {
-                        ctx->set_line_width(0.15);
-                    }
-                    else if(i % 10 == 0) {
-                        ctx->set_line_width(0.05);
-                    }
-                    else {
-                        ctx->set_line_width(0.01);
-                    }
-
-                    ctx->move_to(i,0);
-                    ctx->line_to(i,40);
-                    ctx->stroke();
-
-                    if(i <= 40)
-                    {
-                        ctx->move_to(0,i);
-                        ctx->line_to(45,i);
-                        ctx->stroke();
-                    }
-                }    
-            }
-            ctx->restore();
+            draw_grid(ctx);
 
 
             auto vehicle_data = this->get_vehicle_data();
             for(const auto& entry : vehicle_data) {
                 const auto vehicle_id = entry.first;
-                const auto& vehicle_sensor_timeseries = entry.second;
+                const auto& vehicle_timeseries = entry.second;
 
-                if(vehicle_sensor_timeseries.at("pose_x")->has_new_data(1.0))
+                if(vehicle_timeseries.at("pose_x")->has_new_data(1.0))
                 {
 
-                    // Draw vehicle trajectory
-                    {
-                        vector<double> trajectory_x = vehicle_sensor_timeseries.at("pose_x")->get_last_n_values(100);
-                        vector<double> trajectory_y = vehicle_sensor_timeseries.at("pose_y")->get_last_n_values(100);
-                        for (size_t i = 1; i < trajectory_x.size(); ++i)
-                        {
-                            if(i == 1) ctx->move_to(trajectory_x[i], -trajectory_y[i]);
-                            else ctx->line_to(trajectory_x[i], -trajectory_y[i]);
-                        }
-                        ctx->set_source_rgb(1,0,0);
-                        ctx->set_line_width(0.01);
-                        ctx->stroke();
-                    }
+                    draw_vehicle_past_trajectory(ctx, vehicle_timeseries);
 
                     // Draw vehicle
                     ctx->save();
                     {                        
-                        const double x = vehicle_sensor_timeseries.at("pose_x")->get_latest_value();
-                        const double y = vehicle_sensor_timeseries.at("pose_y")->get_latest_value();
-                        const double yaw = vehicle_sensor_timeseries.at("pose_yaw")->get_latest_value();
+                        const double x = vehicle_timeseries.at("pose_x")->get_latest_value();
+                        const double y = vehicle_timeseries.at("pose_y")->get_latest_value();
+                        const double yaw = vehicle_timeseries.at("pose_yaw")->get_latest_value();
 
-                        ctx->translate(x,-y);
-                        ctx->rotate(-yaw);
+                        ctx->translate(x,y);
+                        ctx->rotate(yaw);
 
                         const double LF = 0.196;
                         const double LR = 0.028;
-                        //const double WH = 0.054;
+                        const double WH = 0.054;
 
                         ctx->save();
                         {
@@ -138,8 +150,8 @@ MapViewUi::MapViewUi(std::function<VehicleData()> get_vehicle_data_callback)
                         {
                             ctx->translate(0.04, 0);
                             const double scale = 0.01;
-                            ctx->rotate(yaw);
-                            ctx->scale(scale, scale);
+                            ctx->rotate(-yaw);
+                            ctx->scale(scale, -scale);
                             ctx->move_to(0,0);
                             Cairo::TextExtents extents;
                             ctx->get_text_extents(to_string(vehicle_id), extents);
@@ -170,6 +182,10 @@ MapViewUi::MapViewUi(std::function<VehicleData()> get_vehicle_data_callback)
         return true;
     });
 }
+
+
+
+
 
 Gtk::DrawingArea* MapViewUi::get_parent()
 {
