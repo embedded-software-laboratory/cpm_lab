cea5c3692622c93a7720e992271545072262002f
leon.rabanus <leon.rabanus@rwth-aachen.de>
2019-04-12 15:57:42 +0200

    CommandLineReader begonnen

---
+++ LabControlCenter/src/CommandLineReader.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <string>
+
+/**
+ * \brief Used to read a single command line argument from argv
+ */
+
+struct CommandLineReader {
+
+    bool auto_start = false;
+    std::string config_file = "";
+
+    void constr(int argc, char *argv[])
+    {
+        for (int i = 1; i < argc; ++i) {
+            std::string param = std::string(argv[i]);
+            if (i + 1 < argc) {
+                if (param.find("-config") != std::string::npos) {
+                    ++i;
+                    this->config_file = std::string(argv[i]);
+                }
+            }
+            else {
+                if (param.find("-autostart") != std::string::npos) {
+                    ++i;
+                    this->auto_start = true;
+                }
+            }
+        }
+    }
+}
--- LabControlCenter/src/main.cpp
+++ LabControlCenter/src/main.cpp
@@ -12,6 +12,7 @@
 #include "ParameterStorage.hpp"
 #include "ui/MainWindow.hpp"
 #include "cpm/Logging.hpp"
+#include "CommandLineReader.hpp"
 
 
 #include <gtkmm/builder.h>
@@ -19,35 +20,42 @@
 
 int main(int argc, char *argv[])
 {
+    //Must be done first, s.t. no class using the logger produces an error
     Logging::Instance().set_id("LabControlCenter");
 
-    ParameterStorage storage("parameters.yaml");
-    ParameterServer server(storage);
+    //Read command line parameters
+    CommandLineReader reader;
+    reader.construct(argc, argv);
+
+    std::cout << "Set autostart to " << reader.auto_start << " and file to " << reader.config_file << std::endl;
+
+    // ParameterStorage storage("parameters.yaml");
+    // ParameterServer server(storage);
 
 
-    Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv);
-    Glib::RefPtr<Gtk::CssProvider> cssProvider = Gtk::CssProvider::create();
-    cssProvider->load_from_path("ui/style.css");
-    Gtk::StyleContext::create()->add_provider_for_screen (Gdk::Display::get_default()->get_default_screen(),cssProvider,500);
+    // Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv);
+    // Glib::RefPtr<Gtk::CssProvider> cssProvider = Gtk::CssProvider::create();
+    // cssProvider->load_from_path("ui/style.css");
+    // Gtk::StyleContext::create()->add_provider_for_screen (Gdk::Display::get_default()->get_default_screen(),cssProvider,500);
 
 
-    auto vehicleManualControl = make_shared<VehicleManualControl>();
-    auto timeSeriesAggregator = make_shared<TimeSeriesAggregator>();
-    auto mapViewUi = make_shared<MapViewUi>([=](){return timeSeriesAggregator->get_vehicle_data();});
-    auto monitoringUi = make_shared<MonitoringUi>([=](){return timeSeriesAggregator->get_vehicle_data();});
-    auto vehicleManualControlUi = make_shared<VehicleManualControlUi>(vehicleManualControl);
-    auto mainWindow = make_shared<MainWindow>(vehicleManualControlUi, monitoringUi, mapViewUi);
+    // auto vehicleManualControl = make_shared<VehicleManualControl>();
+    // auto timeSeriesAggregator = make_shared<TimeSeriesAggregator>();
+    // auto mapViewUi = make_shared<MapViewUi>([=](){return timeSeriesAggregator->get_vehicle_data();});
+    // auto monitoringUi = make_shared<MonitoringUi>([=](){return timeSeriesAggregator->get_vehicle_data();});
+    // auto vehicleManualControlUi = make_shared<VehicleManualControlUi>(vehicleManualControl);
+    // auto mainWindow = make_shared<MainWindow>(vehicleManualControlUi, monitoringUi, mapViewUi);
 
 
-    vehicleManualControl->set_callback([&](){vehicleManualControlUi->update();});
+    // vehicleManualControl->set_callback([&](){vehicleManualControlUi->update();});
 
 
 
 
-    app->signal_startup().connect([&]{
-        // A second window can be added like this:
-        //app->add_window(mapViewUi->get_window());
-    });
+    // app->signal_startup().connect([&]{
+    //     // A second window can be added like this:
+    //     //app->add_window(mapViewUi->get_window());
+    // });
 
-    return app->run(mainWindow->get_window());
+    // return app->run(mainWindow->get_window());
 }
