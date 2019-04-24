e7527b36bf148b4a7871c4af7ba27d8cda75de17
leon.rabanus <leon.rabanus@rwth-aachen.de>
2019-04-24 12:01:53 +0200

    ParameterStorage change coming

--- LabControlCenter/src/ParameterWithDescription.hpp
+++ LabControlCenter/src/ParameterWithDescription.hpp
@@ -1,6 +1,10 @@
 #include "cpm/dds/Parameter.hpp"
 #include <string>
 
+/**
+ * \brief To be used with ParameterStorage, to save parameter information as well as their description 
+ */ 
+
 struct ParameterWithDescription {
     Parameter parameter_data;
     std::string parameter_description;
