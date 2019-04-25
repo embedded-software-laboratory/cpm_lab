37cf39f6a03b98ba3b683b3235f3a7a78301d4dd
Janis Maczijewski <Janis.Maczijewski@rwth-aachen.de>
2019-04-25 13:00:06 +0200

    removing old IPS prototype

--- simple_ips/build.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-clear
-clear
-mkdir build
-cd build
-cmake .. 
-make -j4
-cd ..
--- simple_ips/src/calibration_floor_plane.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-
-void calibration_floor_plane(
-    double image_x,
-    double image_y,
-    double& world_x,
-    double& world_y
-);
