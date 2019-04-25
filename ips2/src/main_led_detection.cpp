9c3e5e78961d808287010de099d588537b221e04
Janis Maczijewski <Janis.Maczijewski@rwth-aachen.de>
2019-04-25 15:33:59 +0200

    image grab and LED detection parallelized

--- ips2/src/main_led_detection.cpp
+++ ips2/src/main_led_detection.cpp
@@ -5,6 +5,7 @@
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <ThreadSafeQueue.hpp>
 
 using namespace Pylon;
 using namespace Basler_UsbCameraParams;
@@ -14,12 +15,7 @@ typedef Pylon::CBaslerUsbInstantCamera Camera_t;
 typedef Pylon::CBaslerUsbImageEventHandler ImageEventHandler_t; // Or use Camera_t::ImageEventHandler_t
 typedef Pylon::CBaslerUsbGrabResultPtr GrabResultPtr_t; // Or use Camera_t::GrabResultPtr_t
 
-
-std::mutex visualization_mutex;
-cv::Mat visualization_image;
-
-
-
+ThreadSafeQueue<cv::Mat> queue_images;
 
 uint64_t get_time_ns()
 {
@@ -28,11 +24,53 @@ uint64_t get_time_ns()
     return uint64_t(t.tv_sec) * 1000000000ull + uint64_t(t.tv_nsec);
 }
 
-int main(int argc, char* argv[])
+void worker_led_detection()
 {
-    // The exit code of the sample application.
-    int exitCode = 0;
+    while (1)
+    {
+        cv::Mat img;
+        queue_images.pop(img);
 
+
+        cv::Mat img_binary;
+        cv::threshold(img, img_binary, 127, 255, cv::THRESH_BINARY);
+
+        std::vector<double> points_x;
+        std::vector<double> points_y;
+        std::vector<std::vector<cv::Point> > contours;
+
+        cv::findContours(img_binary, contours, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE);
+
+        for (std::vector<cv::Point> contour : contours) {
+            double size = cv::contourArea(contour);
+            if (size < 50 && size > 6) {
+                cv::Moments M = cv::moments(contour);
+                points_x.push_back((M.m10 / (M.m00 + 1e-5)));
+                points_y.push_back((M.m01 / (M.m00 + 1e-5)));
+            }
+        }
+
+
+        // Visualization    
+        {
+            cv::Mat img_small;
+            cv::resize(img, img_small, cv::Size(), 0.5, 0.5);
+            cv::Mat img_small_BGR;
+            cv::cvtColor(img_small, img_small_BGR, cv::COLOR_GRAY2BGR);
+
+            for (size_t i = 0; i < points_x.size(); ++i)
+            {
+                cv::circle(img_small_BGR, cv::Point(points_x[i]/2, points_y[i]/2), 7, cv::Scalar(0,0,255));
+            }
+
+            cv::imshow( "", img_small_BGR ); 
+            cv::waitKey(1);
+        }
+    }
+}
+
+void worker_grab_image()
+{
     // Before using any pylon methods, the pylon runtime must be initialized. 
     PylonInitialize();
 
@@ -88,7 +126,7 @@ int main(int argc, char* argv[])
 
         // Set exposure
         camera.ExposureAuto.SetValue(ExposureAuto_Off);
-        camera.ExposureTime.SetValue(80);
+        camera.ExposureTime.SetValue(500);
 
 
         camera.StartGrabbing();
@@ -136,52 +174,9 @@ int main(int argc, char* argv[])
 
 
                 cv::Mat img = (cv::Mat(ptrGrabResult->GetHeight(), ptrGrabResult->GetWidth(), CV_8UC1, pImageBuffer)).clone();
+                queue_images.push(img);
 
-                
-
-                cv::Mat img_binary;
-                cv::threshold(img, img_binary, 127, 255, cv::THRESH_BINARY);
-
-                std::vector<double> points_x;
-                std::vector<double> points_y;
-                std::vector<std::vector<cv::Point> > contours;
-
-                cv::findContours(img_binary, contours, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE);
-
-                for (std::vector<cv::Point> contour : contours) {
-                    double size = cv::contourArea(contour);
-                    if (size < 15 && size > 6) {
-                        cv::Moments M = cv::moments(contour);
-                        points_x.push_back((M.m10 / (M.m00 + 1e-5)));
-                        points_y.push_back((M.m01 / (M.m00 + 1e-5)));
-                    }
-                }
-
-
-                // Visualization
-                
-                {
-                    cv::Mat img_small;
-                    cv::resize(img, img_small, cv::Size(), 0.5, 0.5);
-                    cv::Mat img_small_BGR;
-                    cv::cvtColor(img_small, img_small_BGR, cv::COLOR_GRAY2BGR);
-
-                    for (size_t i = 0; i < points_x.size(); ++i)
-                    {
-                        cv::circle(img_small_BGR, cv::Point(points_x[i]/2, points_y[i]/2), 7, cv::Scalar(0,0,255));
-                    }
-
-                    cv::imshow( "", img_small_BGR ); 
-                    cv::waitKey(1);
-                }
-
-                // Update visualization Data
-                /*{
-                    std::lock_guard<std::mutex> lock(visualization_mutex);
-                    img.copyTo(visualization_image);
-                }*/
-
-
+                // report actual FPS
                 if(frameCount % 100 == 0)
                 {
                     uint64_t now = get_time_ns();
@@ -200,12 +195,15 @@ int main(int argc, char* argv[])
     catch (const GenericException &e)
     {
         cerr << "An exception occurred." << endl << e.GetDescription() << endl;
-        exitCode = 1;
     }
 
-    // Releases all pylon resources. 
     PylonTerminate(); 
-
-    return exitCode;
 }
 
+
+int main(int argc, char* argv[])
+{
+    std::thread thread_led_detection([](){worker_led_detection();});
+    worker_grab_image();
+    return 0;
+}
