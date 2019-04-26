db169f5e6bd9bb5503cc429f50b48557b16d104c
Janis Maczijewski <Janis.Maczijewski@rwth-aachen.de>
2019-04-26 15:09:52 +0200

    receiving led points from replay

--- ips2/src/main_ips_manual_test.cpp
+++ ips2/src/main_ips_manual_test.cpp
@@ -2,15 +2,31 @@
 #include <thread>
 #include <stdlib.h>
 #include <unistd.h>
+#include "cpm/AsyncReader.hpp"
+#include "LedPoints.hpp"
+#include "cpm/ParticipantSingleton.hpp"
+#include "cpm/get_topic.hpp"
+
+
+void process_LedPoints(LedPoints led_points)
+{
+    std::cout << "recvd " << led_points.led_points().size() << " LEDs" << std::endl;
+}
+
 
 int main(int argc, char* argv[])
 {
-    std::thread rti_replay_thread([](){system("rtireplay -cfgName mydefault -cfgFile recordings/replay_config.xml");});
-    
-    while(1)
-    {
-        std::cout << "." << std::endl;
-        usleep(500000);
-    }
+    cpm::AsyncReader<LedPoints> ipsLedPoints_reader(
+        [](dds::sub::LoanedSamples<LedPoints>& samples){
+            for(auto sample : samples) 
+                if(sample.info().valid()) 
+                    process_LedPoints(sample.data());
+        }, 
+        cpm::ParticipantSingleton::Instance(), 
+        cpm::get_topic<LedPoints>("ipsLedPoints")
+    );
+
+    // start replay
+    system("rtireplay -cfgName mydefault -cfgFile recordings/replay_config.xml");    
     return 0;
 }
