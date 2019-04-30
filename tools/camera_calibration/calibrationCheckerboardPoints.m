dfe9069e667cf9a852a13f74afb2cc0edf1d801e
Janis Maczijewski <Janis.Maczijewski@rwth-aachen.de>
2019-04-30 13:11:58 +0200

    new camera calibration

--- tools/camera_calibration/calibrationCheckerboardPoints.m
+++ tools/camera_calibration/calibrationCheckerboardPoints.m
@@ -3,7 +3,7 @@ function calibrationCheckerboardPoints
     if exist('checkerboardPoints.mat','file')
         load checkerboardPoints
     else
-        I=imread('/home/janis/Desktop/Image__2019-03-19__16-12-52.png');
+        I=imread('/home/janis/Desktop/Image__2019-04-30__13-01-44.png');
         [imagePoints,boardSize] = detectCheckerboardPoints(I);
         save checkerboardPoints
     end
@@ -28,10 +28,15 @@ function calibrationCheckerboardPoints
     fclose(f);
 
     clf
+    
+    I = insertText(I,[imagePoints(1,end,1),imagePoints(1,end,2)],'x','FontSize',40);
+    I = insertText(I,[imagePoints(end,1,1),imagePoints(end,1,2)],'y','FontSize',40);
+    I = insertText(I,[imagePoints(1,1,1),imagePoints(1,1,2)],'origin','FontSize',40);
     imshow(I);
     hold on;
     plot(imagePoints(:,:,1),imagePoints(:,:,2),'rx');
     plot(imagePoints(1,1,1),imagePoints(1,1,2),'ro');
+    
 
 end
 
