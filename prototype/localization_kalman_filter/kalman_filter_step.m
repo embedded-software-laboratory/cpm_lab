a4703f2ea82d67ee9eb8e1aa32606296c87e4769
Janis Maczijewski <Janis.Maczijewski@rwth-aachen.de>
2019-05-05 13:56:08 +0200

    cleanup prototypes

--- prototype/localization_kalman_filter/kalman_filter_step.m
+++
@@ -1,8 +0,0 @@
-function [x,P] = kalman_filter_step(x_predicted,z_predicted,z_measured,P,A,C,Q,R)    
-    P = A * P * A' + Q;
-    dz = z_measured - z_predicted;
-    K = (P * C') / (C * P * C' + R);
-    x = x_predicted + K * dz;
-    P = (eye(size(A)) - K * C) * P;
-end
-
--- prototype/middleware_synchronization/clion.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source setup.sh
-
-if [ ! -f src/.idea/workspace.xml ]; then
-    cp src/.idea/workspace.xml.template src/.idea/workspace.xml
-fi
-
-nohup ~/clion/bin/clion.sh src >/dev/null 2>&1 &
