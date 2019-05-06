8e98d81377a865a114b9c30f7643f13341658946
Janis Maczijewski <Janis.Maczijewski@rwth-aachen.de>
2019-05-06 15:18:04 +0200

    WIP controller_test_loop

--- controller_test_loop/src/main.cpp
+++ controller_test_loop/src/main.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 #include <thread>
+#include <map>
 #include <stdlib.h>
 #include <unistd.h>
 #include "cpm/AsyncReader.hpp"
 #include "cpm/ParticipantSingleton.hpp"
+#include "cpm/Timer.hpp"
 #include "cpm/get_topic.hpp"
 #include "VehicleObservation.hpp"
+#include "test_loop_trajectory.hpp"
 
 
 
 int main(int argc, char* argv[])
 {
+    // Receive IPS DDS data
     std::map<uint8_t, VehicleObservation> vehicleObservations;
     std::mutex vehicleObservations_mutex;
 
@@ -18,15 +22,42 @@ int main(int argc, char* argv[])
         [&](dds::sub::LoanedSamples<VehicleObservation>& samples) {
             std::unique_lock<std::mutex> lock(vehicleObservations_mutex);
             for(auto sample : samples) 
+            {
                 if(sample.info().valid())
                 {
                     auto data = sample.data();
                     vehicleObservations[data.vehicle_id()] = data;
                 }
+            }
         }, 
         cpm::ParticipantSingleton::Instance(), 
         cpm::get_topic<VehicleObservation>("vehicleObservation")
     );
+
+
+    // Load trajectory data
+    std::vector<TrajectoryPoint> trajectory_points;
+    uint64_t loop_period_nanoseconds;
+    uint64_t point_period_nanoseconds;
+    uint64_t vehicle_time_gap_nanoseconds;
+    int n_max_vehicles;
+
+    get_test_loop_trajectory(
+        trajectory_points,
+        loop_period_nanoseconds,
+        point_period_nanoseconds,
+        vehicle_time_gap_nanoseconds,
+        n_max_vehicles
+    );
+ 
+
+    // Control loop, which sends commands to the vehicles
+    auto timer = cpm::Timer::create("controller_test_loop", 40000000ull, 0, false, true);
+    timer->start([&](uint64_t t_now) {
+        std::unique_lock<std::mutex> lock(vehicleObservations_mutex);
+
+
+    });
 
     return 0;
 }
