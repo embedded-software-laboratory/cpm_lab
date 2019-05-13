e0300e48651f0af2dc5b1551b7f927e997012892
Janis Maczijewski <Janis.Maczijewski@rwth-aachen.de>
2019-05-13 14:50:32 +0200

    record convert script

---
+++ matlab_scripts/vehicle_dynamics_identification/recording_to_csv.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export LC_NUMERIC="en_US.UTF-8"
+
+mkdir output
+pushd output
+
+
+
+for f in ../recording*.dat_0_0; do
+    export PREFIX="$(echo $f | tr -cd '[:alnum:]')"
+    rtirecconv -format csv -decodeChar text -decodeOctet hex -time epoch -compact no -filePrefix $PREFIX $f
+done
+
