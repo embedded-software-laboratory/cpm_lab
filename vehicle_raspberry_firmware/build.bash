6a1ba7ec3bca887c274e402547baa0dc228781af
Janis Maczijewski <Janis.Maczijewski@rwth-aachen.de>
2019-05-17 15:26:53 +0200

    WIP passive deployment of RPi software

--- vehicle_raspberry_firmware/build.bash
+++ vehicle_raspberry_firmware/build.bash
@@ -16,3 +16,11 @@ cmake .. -DCMAKE_TOOLCHAIN_FILE=../Toolchain.cmake
 make -j4
 cd ..
 
+
+
+
+cp build/vehicle_rpi_firmware package
+cp ../../cpm_base/cpm_lib/build_arm/libcpm.so package
+tar -czvf package.tar.gz package
+rm /var/www/html/raspberry/package.tar.gz
+cp ./package.tar.gz /var/www/html/raspberry
