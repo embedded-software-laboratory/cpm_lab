583a3939a44ce4d765c2de9b82585e025e95061e
Janis Maczijewski <janis.maczijewski@rwth-aachen.de>
2019-06-05 16:50:44 +0200

    dynamics delay optimization evaluation: input lag 4 samples (80ms) wrt IPS, local sensors 0 samples wrt IPS

--- matlab_scripts/vehicle_dynamics_identification_and_mpc/optimize_parameters_delay_grid_eval.m
+++ matlab_scripts/vehicle_dynamics_identification_and_mpc/optimize_parameters_delay_grid_eval.m
@@ -12,6 +12,15 @@ function optimize_parameters_delay_grid_eval
     n_delay_steps_command = [results.n_delay_steps_command];
     objective_soln = [results.objective_soln];
     
+    dim_delay_steps_IPS = numel(unique(n_delay_steps_IPS));
+    dim_delay_steps_local = numel(unique(n_delay_steps_local));
+    dim_delay_steps_command = numel(unique(n_delay_steps_command));
+    
+    N = dim_delay_steps_IPS * dim_delay_steps_local * dim_delay_steps_command;
+    assert(N == numel(n_delay_steps_IPS));
+    assert(N == numel(n_delay_steps_local));
+    assert(N == numel(n_delay_steps_command));
+    
     
     [obj_min, I]=min(objective_soln);
     
@@ -19,6 +28,33 @@ function optimize_parameters_delay_grid_eval
     n_delay_steps_local_opt = n_delay_steps_local(I);
     n_delay_steps_command_opt = n_delay_steps_command(I);
     
-    plot(objective_soln,'o-')
+    objective_soln = reshape(objective_soln, dim_delay_steps_command, dim_delay_steps_local, dim_delay_steps_IPS);
+    
+    
+    objective_soln_cmd = reshape(objective_soln, dim_delay_steps_command, dim_delay_steps_local*dim_delay_steps_IPS);
+    
+    objective_soln_local = permute(objective_soln, [2 1 3]);
+    objective_soln_local = reshape(objective_soln_local, dim_delay_steps_local, dim_delay_steps_command*dim_delay_steps_IPS);
+    
+    objective_soln_IPS = permute(objective_soln, [3 2 1]);
+    objective_soln_IPS = reshape(objective_soln_IPS, dim_delay_steps_IPS, dim_delay_steps_command*dim_delay_steps_local);
+    
+    
+    plot(objective_soln_cmd,'o-')
+    plot(objective_soln_local,'o-')
+    plot(objective_soln_IPS,'o-')
+    
+    
+    cutoff_objective = quantile(objective_soln(:),12/N);
+    filter = objective_soln(:) <= cutoff_objective;
+    
+    relative_delays_local = n_delay_steps_local(filter) - n_delay_steps_IPS(filter);
+    relative_delays_command = n_delay_steps_command(filter) - n_delay_steps_IPS(filter);
+    
 end
+
+
+
+
+
 
