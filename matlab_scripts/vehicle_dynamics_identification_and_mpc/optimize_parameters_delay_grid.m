681e96b75feacde67b5b460143f306ac1f7df13b
Janis Maczijewski <janis.maczijewski@rwth-aachen.de>
2019-06-05 13:20:09 +0200

    larger search area for dynamics delay

--- matlab_scripts/vehicle_dynamics_identification_and_mpc/optimize_parameters_delay_grid.m
+++ matlab_scripts/vehicle_dynamics_identification_and_mpc/optimize_parameters_delay_grid.m
@@ -1,7 +1,7 @@
 function optimize_parameters_delay_grid
-    for n_delay_steps_IPS = 0:3
-        for n_delay_steps_local = 0:3
-            for n_delay_steps_command = 0:5
+    for n_delay_steps_IPS = 0:4
+        for n_delay_steps_local = 0:4
+            for n_delay_steps_command = 0:9
                 tic
                 optimize_parameters(n_delay_steps_IPS, n_delay_steps_local, n_delay_steps_command)
                 toc
