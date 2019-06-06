d0ffb240c261676e2dd36ed904f717ec16f4064f
Janis Maczijewski <janis.maczijewski@rwth-aachen.de>
2019-06-06 17:38:07 +0200

    WIP changing raspberry build for onboard simulation

--- vehicle_raspberry_firmware/build.bash
+++ vehicle_raspberry_firmware/build.bash
@@ -7,19 +7,19 @@ mkdir build_x64_sim
 
 
 pushd build_x64_sim
-cmake .. -DBUILD_X64=ON -DBUILD_SIMULATION=ON
+cmake .. -DBUILD_ARM=OFF -DBUILD_SIMULATION=ON
 make -j20
 popd
 
 
 pushd build_arm_sim
-cmake .. -DBUILD_X64=OFF -DBUILD_SIMULATION=ON -DCMAKE_TOOLCHAIN_FILE=../Toolchain.cmake
+cmake .. -DBUILD_ARM=ON -DBUILD_SIMULATION=ON -DCMAKE_TOOLCHAIN_FILE=../Toolchain.cmake
 make -j20
 popd
 
 
 pushd build_arm
-cmake .. -DBUILD_X64=OFF -DBUILD_SIMULATION=OFF -DCMAKE_TOOLCHAIN_FILE=../Toolchain.cmake
+cmake .. -DBUILD_ARM=ON -DBUILD_SIMULATION=OFF -DCMAKE_TOOLCHAIN_FILE=../Toolchain.cmake
 make -j20
 popd
 
