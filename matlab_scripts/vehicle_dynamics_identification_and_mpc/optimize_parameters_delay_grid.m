7083d69a768005c8b0ddfed5b9c5297505360225
Janis Maczijewski <janis.maczijewski@rwth-aachen.de>
2019-06-06 16:12:28 +0200

    evaluation updated for different motor/steering delay

--- matlab_scripts/vehicle_dynamics_identification_and_mpc/optimize_parameters_delay_grid.m
+++ matlab_scripts/vehicle_dynamics_identification_and_mpc/optimize_parameters_delay_grid.m
@@ -1,8 +1,8 @@
 function optimize_parameters_delay_grid
-    for n_delay_steps_IPS = 0:1
-        for n_delay_steps_local = 0:1
-            for n_delay_steps_steering = 2:5
-                for n_delay_steps_motor = 2:5
+    for n_delay_steps_IPS = 0:3
+        for n_delay_steps_local = 0:3
+            for n_delay_steps_steering = 0:8
+                for n_delay_steps_motor = 0:8
                     tic
                         optimize_parameters(n_delay_steps_IPS, n_delay_steps_local, n_delay_steps_steering, n_delay_steps_motor)
                     toc
