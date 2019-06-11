35142de993b59a6f96f1698cfa30a11919c4d028
leon.rabanus <leon.rabanus@rwth-aachen.de>
2019-06-11 10:53:37 +0200

    read uint64_t command, plus test

--- cpm_lib/include/cpm/CommandLineReader.hpp
+++ cpm_lib/include/cpm/CommandLineReader.hpp
@@ -20,6 +20,11 @@ namespace cpm {
     int cmd_parameter_int(std::string name, int default_value, int argc, char *argv[]);
 
     /**
+     * \brief Read an integer command line argument from argv (form: --name=value), use a default value if it does not exist
+     */
+    uint64_t cmd_parameter_uint64_t(std::string name, uint64_t default_value, int argc, char *argv[]);
+
+    /**
      * \brief Read a std::string command line argument from argv (form: --name=value), use a default value if it does not exist
      */
     std::string cmd_parameter_string(std::string name, std::string default_value, int argc, char *argv[]);
