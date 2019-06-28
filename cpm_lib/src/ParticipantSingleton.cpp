dd584d8ec253cb157476eb7c4238c253a35582d4
Janis Maczijewski <janis.maczijewski@rwth-aachen.de>
2019-06-28 12:56:25 +0200

    no multicast qos

--- cpm_lib/src/ParticipantSingleton.cpp
+++ cpm_lib/src/ParticipantSingleton.cpp
@@ -5,7 +5,17 @@
 namespace cpm 
 {
     dds::domain::DomainParticipant& ParticipantSingleton::Instance() {
-        static dds::domain::DomainParticipant myInstance(cpm::InternalConfiguration::Instance().get_dds_domain());
+
+        static dds::core::QosProvider xml_file_provider(
+            "no_multicast_qos.xml", 
+            "no_multicast_library::no_multicast_profile"
+        );
+
+        static dds::domain::DomainParticipant myInstance(
+            cpm::InternalConfiguration::Instance().get_dds_domain(),
+            xml_file_provider.participant_qos("no_multicast_library::no_multicast_profile")
+        );
+
         return myInstance;
     }
 }
