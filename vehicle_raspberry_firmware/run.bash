e873403d080a5b17383240ac4bcf981c78494c79
Janis Maczijewski <janis.maczijewski@rwth-aachen.de>
2019-07-04 14:39:51 +0200

    RPi controller, fix race condition

--- vehicle_raspberry_firmware/run.bash
+++ vehicle_raspberry_firmware/run.bash
@@ -5,4 +5,4 @@ export IP_SELF="$(hostname -I)"
 export IP_SELF="$(echo $IP_SELF)"
 export DDS_INITIAL_PEER=rtps@udpv4://$IP_SELF:25598
 
-./build_x64_sim/vehicle_rpi_firmware --simulated_time=false --vehicle_id=$1 --dds_domain=$DDS_DOMAIN --dds_initial_peer=$DDS_INITIAL_PEER
+./build_x64_sim/vehicle_rpi_firmware --simulated_time=false --vehicle_id=$1 --dds_domain=$DDS_DOMAIN --dds_initial_peer=$DDS_INITIAL_PEER >stdout_$1.txt 2>stderr_$1.txt
