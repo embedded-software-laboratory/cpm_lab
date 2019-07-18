b36630a80ea865bc881e86b4dcc5ee90d412c992
leon.rabanus <leon.rabanus@rwth-aachen.de>
2019-07-18 12:51:46 +0200

    first implementation

---
+++ LabControlCenter/src/VisualizationCommandsAggregator.cpp
@@ -0,0 +1,41 @@
+#include "VisualizationCommandsAggregator.hpp"
+
+VisualizationCommandsAggregator::VisualizationCommandsAggregator() 
+{
+    viz_reader = make_shared<cpm::AsyncReader<Visualization>>(
+        [this](dds::sub::LoanedSamples<Visualization>& samples){
+            handle_new_viz_msgs(samples);
+        },
+        cpm::ParticipantSingleton::Instance(),
+        cpm::get_topic<Visualization>("visualization")
+    );
+}
+
+void VisualizationCommandsAggregator::handle_new_viz_msgs(dds::sub::LoanedSamples<Visualization>& samples) {
+    for (auto sample : samples) {
+        if (sample.info().valid()) {
+            //Add new message or delete old one using the id
+            if (sample.data().action() == VisualizationAction::ADD) {
+                std::lock_guard<std::mutex> lock(received_viz_map_mutex);
+
+                received_viz_map[sample.data().id()] = sample.data();
+            }
+            else {
+                std::lock_guard<std::mutex> lock(received_viz_map_mutex);
+
+                received_viz_map.erase(sample.data().id());
+            }
+        }
+    }
+}
+
+std::vector<Visualization> VisualizationCommandsAggregator::get_all_visualization_messages() {
+    std::vector<Visualization> viz_vector;
+    
+    std::lock_guard<std::mutex> lock(received_viz_map_mutex);
+    for (std::map<long, Visualization>::iterator it = received_viz_map.begin(); it != received_viz_map.end(); ++it) {
+        viz_vector.push_back(it->second);
+    }
+
+    return viz_vector;
+}
--- LabControlCenter/src/VisualizationCommandsAggregator.hpp
+++ LabControlCenter/src/VisualizationCommandsAggregator.hpp
@@ -1,8 +1,13 @@
 #pragma once
 
+#include "defaults.hpp"
+#include <map>
 #include <mutex>
 #include <vector>
 
+#include "cpm/AsyncReader.hpp"
+#include "cpm/ParticipantSingleton.hpp"
+#include "cpm/get_topic.hpp"
 #include "Visualization.hpp"
 
 /**
@@ -11,10 +16,20 @@
 
 class VisualizationCommandsAggregator {
 private:
+    /**
+     * \brief Stores new viz messages or deletes old ones, depending on their action
+     */
+    void handle_new_viz_msgs(dds::sub::LoanedSamples<Visualization>& samples);
+
+    //Reader to receive samples, map / mutex to thread-safely store and access them
+    std::shared_ptr<cpm::AsyncReader<Visualization>> viz_reader;
     std::map<long, Visualization> received_viz_map;
     std::mutex received_viz_map_mutex;
 public:
     VisualizationCommandsAggregator();
 
+    /**
+     * \brief Returns all viz messages that have been received
+     */
     std::vector<Visualization> get_all_visualization_messages();
 };
