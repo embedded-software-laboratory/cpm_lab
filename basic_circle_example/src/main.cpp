335d3f0a2fa240c5a293cfae5a390159fbe20709
CPM Lab PC <cpm@example.com>
2019-09-12 12:06:36 +0200

    WIP: Circle with 4 trajectory points

--- basic_circle_example/src/main.cpp
+++ basic_circle_example/src/main.cpp
@@ -33,9 +33,7 @@ int main(int argc, char *argv[])
         cpm::get_topic<VehicleCommandTrajectory>("vehicleCommandTrajectory")
     );
 
-    double map_center_x = 2.25;
-    double map_center_y = 2.0;
-    int num_trajectory_points = 5;
+    int num_trajectory_points = 10;
     uint64_t t_start = 0;
     auto timer = cpm::Timer::create(node_id, dt_nanos, 0, false, true, enable_simulated_time);
 
@@ -46,6 +44,8 @@ int main(int argc, char *argv[])
     vector<double> circle_vx = vector<double>{ 0,-1, 0, 1};
     vector<double> circle_vy = vector<double>{ 1, 0,-1, 0};
 
+    double map_center_x = 2.25;
+    double map_center_y = 2.0;
     for (double &px : circle_px)
     {
         px += map_center_x;
@@ -56,16 +56,32 @@ int main(int argc, char *argv[])
     }
 
     // duration ~= PI*d/4
-    vector<double> t_section = vector<double>{ 0, 1.55, 3.1, 4.65};
-    double t_cycle = t_section.back();
+    // vector<double> t_section = vector<double>{1.55, 1.55, 1.55, 1.55};
+    // vector<double> t_section_added = t_section;
+    // double t_cycle = 0;
+
+    // for (uint i = 1; i < t_section_added.size(); ++i)
+    // {
+    //     t_section_added[i] += t_section_added[i-1];
+    // }
+    // for (double &t : t_section)
+    // {
+    //     t_cycle += t;
+    // }
+    vector<uint64_t> t_section = vector<uint64_t>{1550000000ull, 1550000000ull, 1550000000ull, 1550000000ull};
+    vector<uint64_t> t_section_added = t_section;
+
+    for (uint i = 1; i < t_section_added.size(); ++i)
+    {
+        t_section_added[i] += t_section_added[i-1];
+    }
+    uint64_t t_cycle = t_section_added.back();
 
     timer->start([&](uint64_t t_now)
     {
         // time used for trajectory generation
-        if (t_start == 0)
-        {
-            t_start = t_now;
-        }
+        if (t_start == 0) t_start = t_now;
+
         VehicleCommandTrajectory vehicle_command_trajectory;
         vehicle_command_trajectory.vehicle_id(vehicle_id);
         vector<TrajectoryPoint> trajectory_points;
@@ -73,29 +89,30 @@ int main(int argc, char *argv[])
         for (int i = 0; i < num_trajectory_points; ++i)
         {
             TrajectoryPoint trajectory_point;
-            uint64_t time_point = t_now - t_start + dt_nanos*i;
-
-            // was muss ich addieren
-            // 
+            uint64_t time_point_nanos = t_now - t_start + dt_nanos*i;
 
             // find 'section' in which vehicle currently moves
-            double t_rel = fmod(time_point*1e-9, t_cycle);
-            auto it_sec = std::upper_bound(t_section.begin(), t_section.end(), t_rel);
-            uint section_idx = it_sec - t_section.begin();
+            uint64_t t_rel = time_point_nanos % t_cycle;
+            auto it_sec = std::upper_bound(t_section_added.begin(), t_section_added.end(), t_rel);
+            uint section_idx = it_sec - t_section_added.begin();
 
+            // timestamp for next trajectory point
+            uint64_t t_next_traj_pt_nanos = 
+                time_point_nanos
+                + t_start
+                + t_section_added[section_idx]
+                - t_rel;
 
-            double t_at_next_point = t_now + t_sec - t_rel + i*dt_nanos;
-            if (section_idx == 5)
-            {
-                section_idx = 0;
-            }
+            trajectory_point.t().nanoseconds(t_next_traj_pt_nanos);
             trajectory_point.px(circle_px[section_idx]);
             trajectory_point.py(circle_py[section_idx]);
             trajectory_point.vx(circle_vx[section_idx]);
             trajectory_point.vy(circle_vy[section_idx]);
             trajectory_points.push_back(trajectory_point);
+            std::cout << trajectory_point << std::endl;
         }
         vehicle_command_trajectory.trajectory_points(trajectory_points);
         writer_vehicleCommandTrajectory.write(vehicle_command_trajectory);
+        std::cout << std::endl;
     });
 }
