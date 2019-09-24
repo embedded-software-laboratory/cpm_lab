65c62c2ab4161b9e14f73002638ee6a5bb49df82
Patrick Scheffe <scheffe@embedded.rwth-aachen.de>
2019-09-24 16:28:56 +0200

    Log warnings if model is not valid anymore

--- vehicle_raspberry_firmware/src/VehicleModel.cxx
+++ vehicle_raspberry_firmware/src/VehicleModel.cxx
@@ -2,10 +2,10 @@
 
 void VehicleModel::step(
     const std::vector<double> &dynamics_parameters,
-    const double &dt,
-    const double &motor_throttle,
-    const double &steering_servo,
-    const double &battery_voltage,
+    const double dt,
+    const double motor_throttle,
+    const double steering_servo,
+    const double battery_voltage,
     double &px, double &py, double &yaw, double &speed)
 {
     double d_px, d_py, d_yaw, d_speed;
@@ -23,10 +23,10 @@ void VehicleModel::step(
 
 void VehicleModel::step(
     const std::vector<double> &dynamics_parameters,
-    const double &dt,
-    const double &motor_throttle,
-    const double &steering_servo,
-    const double &battery_voltage,
+    const double dt,
+    const double motor_throttle,
+    const double steering_servo,
+    const double battery_voltage,
     double &px, double &py, double &yaw, double &speed,
     double &d_px, double &d_py, double &d_yaw, double &d_speed)
 {
@@ -44,14 +44,50 @@ void VehicleModel::step(
     yaw   += dt * d_yaw;
     speed += dt * d_speed;
 
-    // Log warnings if model is not valid
-    // input
-    //      motor
-    //      steering
-    //      battery
-    // state
-    //      d_yaw and speed
-    //      d_speed
-    
+    log_sim_warnings(motor_throttle, steering_servo, speed, d_speed, d_yaw);
+        
     return;
+}
+
+
+void VehicleModel::log_sim_warnings(
+    const double motor_throttle,
+    const double steering_servo,
+    const double speed,
+    const double d_speed,
+    const double d_yaw)
+{
+    // Log warnings if model is not valid
+    double motor_throttle_lim = 1.0;
+    double steering_servo_lim = 1.0;
+    // TODO: sensible values; a ~= mu*g?
+    double a_lon_lim = 9.81;
+    double a_lat_lim = 9.81;
+    // input for motor and steering
+    if (fabs(motor_throttle) > motor_throttle_lim)
+    {
+        cpm::Logging::Instance().write(
+            "Absolute of motor_throttle=%f exceeds limit of %f.", 
+            motor_throttle, motor_throttle_lim);     
+    }
+    if (fabs(steering_servo) > steering_servo_lim)
+    {
+        cpm::Logging::Instance().write(
+            "Absolute of steering_servo=%f exceeds limit of %f.", 
+            steering_servo, steering_servo_lim);     
+    }
+    // longitudinal and lateral acceleration
+    if (fabs(d_speed) > a_lon_lim)
+    {
+        cpm::Logging::Instance().write(
+            "Absolute of longitudinal acceleration %f exceeds limit of %f.", 
+            d_speed, a_lon_lim);
+    }
+    double a_lat = speed*d_yaw;
+    if (fabs(a_lat) > a_lat_lim)
+    {
+        cpm::Logging::Instance().write(
+            "Absolute of lateral acceleration %f exceeds limit of %f.", 
+            a_lat, a_lat_lim);
+    }
 }
