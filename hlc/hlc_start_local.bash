217ebcab8a5af205e767cd78ff7ced05667440e8
CPM Lab PC <cpm@example.com>
2019-10-01 10:51:28 +0200

    Further small changes for new NUC system

--- hlc/hlc_start_local.bash
+++ hlc/hlc_start_local.bash
@@ -6,4 +6,4 @@ vehicle_id=$3
 #Load environment variables, like RTI location, library location, Matlab location...
 source ./environment_variables.bash
 
-/opt/MATLAB/R2018b/bin/matlab -nodisplay -nosplash -logfile matlab.log -nodesktop -r "cd '~/dev/software/hlc/${script_path}'; ${script_name}(1, '${vehicle_id}')" #1 is the local comm. domain ID, cannot be changed currently (is probably also not necessary)
+/opt/MATLAB/R2019a/bin/matlab -nodisplay -nosplash -logfile matlab.log -nodesktop -r "cd '~/dev/software/hlc/${script_path}'; ${script_name}(1, '${vehicle_id}')" #1 is the local comm. domain ID, cannot be changed currently (is probably also not necessary)
