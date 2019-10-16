1ad896a7e910ee8dcf5564162190c6c4bad56e1e
Patrick Scheffe <scheffe@embedded.rwth-aachen.de>
2019-10-16 17:09:39 +0200

    removed actuator state

--- vehicle_raspberry_firmware/src/Controller.cxx
+++ vehicle_raspberry_firmware/src/Controller.cxx
@@ -352,6 +352,6 @@ void Controller::get_control_signals(uint64_t t_now, double &out_motor_throttle,
     motor_throttle = fmax(-1.0, fmin(1.0, motor_throttle));
     steering_servo = fmax(-1.0, fmin(1.0, steering_servo));
 
-    out_motor_throttle = motor_throttle_state;
-    out_steering_servo = steering_servo_state;
+    out_motor_throttle = motor_throttle;
+    out_steering_servo = steering_servo;
 }
