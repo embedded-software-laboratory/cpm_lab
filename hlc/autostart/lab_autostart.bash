b84a396d04eba0cbcf64986110305d717273152b
CPM Lab PC <cpm@example.com>
2019-11-22 16:18:27 +0100

    Finaler fix

--- hlc/autostart/lab_autostart.bash
+++ hlc/autostart/lab_autostart.bash
@@ -1,6 +1,6 @@
-#!bin/bash
+#!/bin/bash
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib64/:./cpm:/opt/rti_connext_dds-6.0.0/lib/x64Linux4gcc7.3.0
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib64/:~/dev/autostart/cpm:/opt/rti_connext_dds-6.0.0/lib/x64Linux4gcc7.3.0
 export PATH=$PATH:$HOME/bin:$HOME/.local/bin:/opt/rti_connext_dds-6.0.0:/opt/rti_connext_dds-6.0.0/bin:/opt/rti_connext_dds-6.0.0/lib/x64Linux4gcc7.3.0:/opt/rti_connext_dds-6.0.0/bin:/opt/raspbian-toolchain-gcc-4.7.2-linux64/bin
 export NDDSHOME=/opt/rti_connext_dds-6.0.0
 export RASPBIAN_TOOLCHAIN=/opt/raspbian-toolchain-gcc-4.7.2-linux64
@@ -9,4 +9,4 @@ export RTI_LICENSE_FILE=/opt/rti_connext_dds-6.0.0/rti_license.dat
 export DDS_INITIAL_PEER=rtps@udpv4://192.168.1.249:25598
 
 # Default domain is 21, just like the vehicle default domain (-> domain for real lab tests)
-./autostart --dds_domain=21 --dds_initial_peer=$DDS_INITIAL_PEER
+~/dev/autostart/autostart --dds_domain=21 --dds_initial_peer=$DDS_INITIAL_PEER
