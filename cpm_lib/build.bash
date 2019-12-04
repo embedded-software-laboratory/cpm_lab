a4f3bd5b03fc585d9f105d6bedb0ef65d114e8ee
Alexandru Kampmann <kampmann@embedded.rwth-aachen.de>
2019-12-04 10:34:18 +0100

    fix for issue 13

--- cpm_lib/build.bash
+++ cpm_lib/build.bash
@@ -1,27 +1,29 @@
 #!/bin/bash
 
-if [ ! -d "build" ]; then
-    mkdir build
-fi
+# DIR holds the location of build.bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Check if apache is installed
+[ ! -d "/var/www/html/" ] && echo "apache2 not installed..aborting, install via 'sudo apt-get install apache2'" && exit
+
+sudo mkdir -p "/var/www/html/nuc"
+mkdir -p $DIR/build
 
 if [ ! -d "dds" ]; then
     ./rtigen.bash
 fi
 
-cd build
-cmake .. 
-make -j8
-cd ..
+cd $DIR/build
+cmake ..
+make -C $DIR/build -j8
 
 # Publish cpm_library package via http/apache for the HLCs to download
-cd /tmp
-mkdir cpm_library_package
-cp -R /home/cpm/dev/cpm_base/dds_idl/ ./cpm_library_package
-cp /home/cpm/dev/cpm_base/cpm_lib/build/libcpm.so ./cpm_library_package
-tar -czvf cpm_library_package.tar.gz cpm_library_package
-rm -f /var/www/html/nuc/cpm_library_package.tar.gz
-cp ./cpm_library_package.tar.gz /var/www/html/nuc
+rm -rf $DIR/cpm_library_package
+mkdir $DIR/cpm_library_package
+cp -R $DIR/../dds_idl/ $DIR/cpm_library_package
+cp $DIR/../cpm_lib/build/libcpm.so $DIR/cpm_library_package
+tar -czvf cpm_library_package.tar.gz $DIR/cpm_library_package
+sudo rm -f /var/www/html/nuc/cpm_library_package.tar.gz
+sudo mv $DIR/cpm_library_package.tar.gz /var/www/html/nuc
 
-cd /home/cpm/dev/cpm_base/cpm_lib/build
-./unittest
-cd ..
+$DIR/build/unittest
