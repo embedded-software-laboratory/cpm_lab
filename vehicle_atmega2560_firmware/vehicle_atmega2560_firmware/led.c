edd5f6f5e8e90d52b7b7c489d37e70313419e7ef
CPM Lab PC <cpm@example.com>
2019-12-04 11:04:29 +0100

    timer ISR for id LED

--- vehicle_atmega2560_firmware/vehicle_atmega2560_firmware/led.c
+++ vehicle_atmega2560_firmware/vehicle_atmega2560_firmware/led.c
@@ -16,7 +16,7 @@ void led_set_state(spi_mosi_data_t* spi_mosi_data) {
 	CLEAR_BIT(PORTC, 0);
 	CLEAR_BIT(PORTC, 1);
 	CLEAR_BIT(PORTC, 2);
-	CLEAR_BIT(PORTC, 7);
+	//CLEAR_BIT(PORTC, 7);
 	
 	if(spi_mosi_data->LED1_enabled) {
 		SET_BIT(PORTC, 0);
@@ -30,9 +30,9 @@ void led_set_state(spi_mosi_data_t* spi_mosi_data) {
 		SET_BIT(PORTC, 2);
 	}
 
-	if(spi_mosi_data->LED4_enabled) {
-		SET_BIT(PORTC, 7);
-	}
+	//if(spi_mosi_data->LED4_enabled) {
+	//	SET_BIT(PORTC, 7);
+	//}
 }
 
 
@@ -41,4 +41,43 @@ void led_setup() {
 	SET_BIT(DDRC, 1);
 	SET_BIT(DDRC, 2);
 	SET_BIT(DDRC, 7);
+
+	// Using timer 5
+	
+	// Fast PWM
+	SET_BIT(TCCR5B, WGM53);
+	SET_BIT(TCCR5B, WGM52);
+	SET_BIT(TCCR5A, WGM51);
+	SET_BIT(TCCR5A, WGM50);
+	
+	// Output on Pin 7 / OC5C / PE5
+	SET_BIT(DDRE, 5);
+	SET_BIT(TCCR5A, COM5C1);
+	
+	// prescaler /8 => 2 MHz
+	SET_BIT(TCCR5B, CS51);
+	
+	// set frequency to 50Hz, unit: 0.5 usec
+	OCR5A = 40000-1;
+	
+	// servo center position (1.5 msec)
+	OCR5C = 3000;
+	
+	// TOP interrupt for tick timer
+	// 50Hz i.e. 20ms
+	SET_BIT(TIMSK5, TOIE5);
+	SET_BIT(TIFR5, TOV5);
+}
+
+ISR(TIMER5_CAPT_vec)
+{
+	tick_count++;
+	if(tick_count % identification_LED_period_ticks[vehicle_id] < identification_LED_enabled_ticks[vehicle_id])
+        {
+            SET_BIT(PORTC, 7);
+        }
+	else
+		{
+			CLEAR_BIT(PORTC, 7);
+		}
 }
