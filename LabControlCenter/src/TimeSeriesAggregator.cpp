b4fc510ea6adf87da64cc273cb91ce0b4a7ad98c
CPM Lab PC <cpm@example.com>
2019-12-09 11:00:17 +0100

    LCC only shows battery level, clock delta, speed and position. TODO: monitor position diff

--- LabControlCenter/src/TimeSeriesAggregator.cpp
+++ LabControlCenter/src/TimeSeriesAggregator.cpp
@@ -54,26 +54,11 @@ void TimeSeriesAggregator::create_vehicle_timeseries(uint8_t vehicle_id)
     timeseries_vehicles[vehicle_id]["ips_yaw"] = make_shared<TimeSeries>(
         "IPS Yaw", "%6.3f", "rad");
 
-    timeseries_vehicles[vehicle_id]["odometer_distance"] = make_shared<TimeSeries>(
-        "Odometer Distance", "%7.2f", "m");
-
-    timeseries_vehicles[vehicle_id]["imu_acceleration_forward"] = make_shared<TimeSeries>(
-        "Acceleration Forward", "%4.1f", "m/s^2");
-
-    timeseries_vehicles[vehicle_id]["imu_acceleration_left"] = make_shared<TimeSeries>(
-        "Acceleration Left", "%4.1f", "m/s^2");
-
     timeseries_vehicles[vehicle_id]["speed"] = make_shared<TimeSeries>(
         "Speed", "%5.2f", "m/s");
-
-    timeseries_vehicles[vehicle_id]["battery_voltage"] = make_shared<TimeSeries>(
-        "Battery Voltage", "%5.2f", "V");
     
     timeseries_vehicles[vehicle_id]["battery_level"] = make_shared<TimeSeries>(
         "Battery Level", "%3.0f", "%");
-
-    timeseries_vehicles[vehicle_id]["motor_current"] = make_shared<TimeSeries>(
-        "Motor Current", "%5.2f", "A");
 
     timeseries_vehicles[vehicle_id]["clock_delta"] = make_shared<TimeSeries>(
         "Clock Delta", "%5.1f", "ms");
@@ -116,13 +101,8 @@ void TimeSeriesAggregator::handle_new_vehicleState_samples(dds::sub::LoanedSampl
             timeseries_vehicles[state.vehicle_id()]["pose_x"]                   ->push_sample(now, state.pose().x());
             timeseries_vehicles[state.vehicle_id()]["pose_y"]                   ->push_sample(now, state.pose().y());
             timeseries_vehicles[state.vehicle_id()]["pose_yaw"]                 ->push_sample(now, state.pose().yaw());
-            timeseries_vehicles[state.vehicle_id()]["odometer_distance"]        ->push_sample(now, state.odometer_distance());
-            timeseries_vehicles[state.vehicle_id()]["imu_acceleration_forward"] ->push_sample(now, state.imu_acceleration_forward());
-            timeseries_vehicles[state.vehicle_id()]["imu_acceleration_left"]    ->push_sample(now, state.imu_acceleration_left());
             timeseries_vehicles[state.vehicle_id()]["speed"]                    ->push_sample(now, state.speed());
-            timeseries_vehicles[state.vehicle_id()]["battery_voltage"]          ->push_sample(now, state.battery_voltage());
             timeseries_vehicles[state.vehicle_id()]["battery_level"]            ->push_sample(now, voltage_to_percent(state.battery_voltage()));
-            timeseries_vehicles[state.vehicle_id()]["motor_current"]            ->push_sample(now, state.motor_current());
             timeseries_vehicles[state.vehicle_id()]["clock_delta"]              ->push_sample(now, double(int64_t(now)- int64_t(state.header().create_stamp().nanoseconds()))/1e6 );
         }
     }
