86acfc6ed96d10e4a2f4854179293fe0ad91b6ba
Patrick Scheffe <scheffe@embedded.rwth-aachen.de>
2020-01-02 13:26:36 +0100

    cleaner build.bash

--- cpm_lib/build.bash
+++ cpm_lib/build.bash
@@ -2,46 +2,38 @@
 
 # DIR holds the location of build.bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-echo $DIR
 
-apache_installed="true"
-
-# Check if apache is installed
-[ ! -d "/var/www/html/" ] && echo "WARNING: apache2 not installed - vehicles / hlcs will not be able to download if this is the master pc, install via 'sudo apt-get install apache2'" && apache_installed="false"
-
-mkdir -p $DIR/build
+cd $DIR
 
 if [ ! -d "dds" ]; then
     echo "Generating C++ IDL files..."
     ./rtigen.bash
 fi
 
-if [ ! -d "dds_idl_matlab" ] 
-then
-    # Path required for Matlab to know NDDS libraries (was shown by matlab)
+if [ ! -d "dds_idl_matlab" ]; then
     echo "Generating Matlab IDL files..."
-    /opt/MATLAB/R2019a/bin/matlab -sd "./" -batch "rtigen_matlab"
+    matlab -sd "./" -batch "rtigen_matlab"
 fi
+
+mkdir -p $DIR/build
 
 cd $DIR/build
 cmake ..
-make -C $DIR/build -j8
+make -C $DIR/build -j8 && $DIR/build/unittest
+cd $DIR
 
-if [[ $apache_installed = "true" ]]
-then
-    sudo mkdir -p "/var/www/html/nuc"
-
+if [ -d "/var/www/html/" ]; then
+    # Apache installed
     # Publish cpm_library package via http/apache for the HLCs to download
-    rm -rf $DIR/build/cpm_library_package
-    mkdir $DIR/build/cpm_library_package
-    cp -R $DIR/../dds_idl/ $DIR/build/cpm_library_package
-    cp -R $DIR/dds_idl_matlab/ $DIR/build/cpm_library_package
-    cp $DIR/build/libcpm.so $DIR/build/cpm_library_package
-    tar -czvf cpm_library_package.tar.gz -C $DIR/build/ cpm_library_package
+    sudo mkdir -p "/var/www/html/nuc"
+    rm -rf $DIR/cpm_library_package
+    mkdir $DIR/cpm_library_package
+    cp -R $DIR/../dds_idl/ $DIR/cpm_library_package
+    cp -R $DIR/dds_idl_matlab/ $DIR/cpm_library_package
+    cp $DIR/libcpm.so $DIR/cpm_library_package
+    tar -czvf cpm_library_package.tar.gz -C $DIR/ cpm_library_package
     sudo rm -f /var/www/html/nuc/cpm_library_package.tar.gz
-    sudo mv $DIR/build/cpm_library_package.tar.gz /var/www/html/nuc
-
-    $DIR/build/unittest
+    sudo mv $DIR/cpm_library_package.tar.gz /var/www/html/nuc
 else
     echo "WARNING: apache2 not installed - vehicles / hlcs will not be able to download if this is the master pc, install via 'sudo apt-get install apache2'"
 fi
--- cpm_lib/rtigen_matlab.m
+++ cpm_lib/rtigen_matlab.m
@@ -1,20 +1,19 @@
 function rtigen_matlab
     setenv("LD_LIBRARY_PATH", ['/opt/rti_connext_dds-6.0.0/lib/x64Linux4gcc7.3.0']);
-    dds_idl_matlab = 'dds_idl_matlab';
+    dds_idl_matlab_dir = 'dds_idl_matlab';
 
     % IDL files from cpm library
-    cpm_idl_directory = fullfile('../dds_idl');
-    if ~exist(cpm_idl_directory, 'dir')
-        error(['Missing directory "' cpm_idl_directory '"']);
+    dds_idl_dir = fullfile('../dds_idl');
+    if ~exist(dds_idl_dir, 'dir')
+        error(['Missing directory "' dds_idl_dir '"']);
     end
-    addpath(cpm_idl_directory)
+    addpath(dds_idl_dir)
 
     % import idl files
-    idl_files = [dir(fullfile(cpm_idl_directory, '*.idl'))];
-    mkdir(dds_idl_matlab);
-    cd(dds_idl_matlab);
+    idl_files = [dir(fullfile(dds_idl_dir, '*.idl'))];
+    mkdir(dds_idl_matlab_dir);
+    cd(dds_idl_matlab_dir);
     for f = {idl_files.name}
         DDS.import(f{1},'matlab', 'f')
     end
-    cd('..')
 end
