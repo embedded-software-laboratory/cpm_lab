652a77c4c76f462e09ee151c6cd156878a133208
leon <leon>
2020-01-29 14:40:13 +0100

    Added error logger for NUCs

--- hlc/autostart/src_error_logger/main.cpp
+++ hlc/autostart/src_error_logger/main.cpp
@@ -5,10 +5,11 @@
  * -> This software already should be installed on the NUCs, in the same folder as lab_autostart.bash 
  */
 
+#include <experimental/filesystem>
+#include <functional>
 #include <memory>
 #include <sstream>
 #include <string>
-#include <functional>
 
 #include <dds/pub/ddspub.hpp>
 
@@ -27,12 +28,17 @@
 #include <ifaddrs.h>
 #include <stdio.h>
 
+bool file_exists(const std::string &filename)
+{
+    return std::experimental::filesystem::exists(filename.c_str());
+}
+
 int main (int argc, char *argv[]) { 
     //Initialize the cpm logger, set domain id etc
     cpm::init(argc, argv);
-    cpm::Logging::Instance().set_id("hlc_hello"); 
+    cpm::Logging::Instance().set_id("hlc_error_logger"); 
 
-    //Write a message every 10 seconds telling that this NUC does not have all required packages
+    //Write a message every 10 seconds telling that this NUC does not have all required packages (if that's the case)
     uint64_t callback_period = 10000000000ull;
 
     //Initialize the timer
@@ -81,10 +87,43 @@ int main (int argc, char *argv[]) {
     std::cout << "Set ID to " << hlc_id << std::endl;
 
     timer->start([&](uint64_t t_now) {
-        
-    },
-    [](){
-        cpm::Logging::Instance().write("NUC %s could not download all required packages from the master PC (maybe some are missing?)", hlc_id);
+        if (!std::experimental::filesystem::is_directory("/home/guest/dev/cpm_base/cpm_lib/dds_idl_matlab"))
+        {
+            cpm::Logging::Instance().write("The cpm IDL files for matlab are missing on NUC %s", hlc_id);
+        }
+
+        if (!file_exists("/home/guest/dev/cpm_base/cpm_lib/build/libcpm.so"))
+        {
+            cpm::Logging::Instance().write("The cpm library is missing on NUC %s", hlc_id);
+        }
+
+        if (!file_exists("/home/guest/dev/software/hlc/middleware/build/middleware"))
+        {
+            cpm::Logging::Instance().write("The middleware executable is missing on NUC %s", hlc_id);
+        }
+
+        if (!file_exists("/home/guest/dev/software/hlc/middleware/build/QOS_LOCAL_COMMUNICATION.xml"))
+        {
+            cpm::Logging::Instance().write("The middleware QoS file is missing on NUC %s", hlc_id);
+        }
+
+        if (!file_exists("/home/guest/dev/software/hlc/import_dds_idl.m"))
+        {
+            cpm::Logging::Instance().write("The matlab import file is missing on NUC %s", hlc_id);
+        }
+
+        if (!file_exists("/home/guest/dev/software/hlc/init_script.m"))
+        {
+            cpm::Logging::Instance().write("The matlab import file is missing on NUC %s", hlc_id);
+        }
+
+        if (!file_exists("/home/guest/dev/software/hlc/QOS_READY_TRIGGER.xml"))
+        {
+            cpm::Logging::Instance().write("The matlab QoS file is missing on NUC %s", hlc_id);
+        }
+
+        //This software only is started in case that packages are missing - if none of the conditions above hold, still log a general error message
+        cpm::Logging::Instance().write("Files are missing on NUC %s", hlc_id);
     });
 
     return 0;
