86ee20a0561c3f0f22334f36fe9e4620731537ae
leon <leon>
2020-03-06 16:47:20 +0100

    Switched to other data structure in Reader

--- cpm_lib/include/cpm/Reader.hpp
+++ cpm_lib/include/cpm/Reader.hpp
@@ -33,34 +33,26 @@ namespace cpm
         dds::sub::DataReader<T> dds_reader;
         std::mutex m_mutex;
 
-        std::vector<const T> messages_buffer;
+        std::vector<T> messages_buffer;
 
         /**
          * \brief Store all received messages since the last call to get_samples in the data structure
          * The current time is used to determine whether a message should be stored at all
          * \param t_now Current time
          */
-        void flush_dds_reader(const uint64_t& t_now)
+        void flush_dds_reader()
         {
             auto samples = dds_reader.take();
+
+            //Just store all relevant data
+            //@Reviewer: What if this function is called e.g. only once a minute and we receive a lot of data?
+            //Should we already filter here, e.g. only store data that is not older than x seconds?
+            //x could also be specified by the user (-> constructor)
             for(auto it = samples.begin(); it != samples.end(); ++it)
             {
                 auto& sample = *it;
                 if(sample.info().valid()) 
                 {
-                    //Add the sample only if no newer sample has been received that is already valid
-                    if (std::next(it, 1) != samples.end())
-                    {
-                        auto& next_sample = *(std::next(it, 1));
-                        if (next_sample.data().header().valid_after_stamp().nanoseconds() <= t_now)
-                            continue;
-                    }
-
-                    //ACHTUNG: Zurzeit falsch, die Werte müssen nicht sortiert sein !!!!!!!!!!!!!
-                    // -> Finde bessere Lösung, vgl. remove_old_messages, wo der neueste Zeitpunkt genutzt wird
-                    // Das muss auch noch geändert werden, denn dort wird zurzeit auch noch t_now verwendet / bzw es scheint so
-                    // -> Vergleiche aktuelle Daten mit größter valider Zeit!
-
                     messages_buffer.push_back(sample.data());
                 }
             }
@@ -68,29 +60,62 @@ namespace cpm
 
         /**
          * \brief Remove all old messages since the last call to get_samples in the data structure
+         * \param current_newest_sample The currently newest sample, as determined by get_newest_sample
          */
-        void remove_old_msgs(const uint64_t& t_now)
+        void remove_old_msgs(const T& current_newest_sample)
         {
-            //First, find out the newest 
+            std::lock_guard<std::mutex> lock(m_mutex);
+
+            //Delete all messages that are older than the currently newest sample
+            //Take a look at the create_stamp only for this
+            //We do this because we do not need these messages anymore, and as they take up space
             auto it = messages_buffer.begin();
             while (it != messages_buffer.end())
             {
                 auto& msg = *it;
-                //Remove the sample only if a newer sample is available
-                if (std::next(it, 1) != samples.end())
+                //Remove the sample only if the currently newest sample is newer regarding its creation
+                if (msg.header().create_stamp().nanoseconds() <= current_newest_sample.header().create_stamp().nanoseconds())
                 {
-                    auto& next_sample = *(std::next(it, 1));
-                    if (next_sample.data().header().valid_after_stamp().nanoseconds() <= t_now)
-                    {
-                        //Remove the msg, get a new iterator to the next position to proceed
-                        it = messages_buffer.erase(it);
-                    }
-                    else
-                    {
-                        //Get the iterator to the next position / proceed to check the age of the next element
-                        ++it;
-                    }
-                    
+                    //Remove the msg, get a new iterator to the next position to proceed
+                    it = messages_buffer.erase(it);
+                }
+                else
+                {
+                    //Get the iterator to the next position / proceed to check the age of the next element
+                    ++it;
+                }
+            }
+        }
+
+        /**
+         * \brief A function that determines the currently newest sample in the buffer
+         * Newest means: Sample with the highest create_stamp that is already valid according to t_now
+         * \param t_now Used to determine which samples are already valid
+         * \param sample_out Return value, either initialized with zeros if no samples exist, else the currently newest sample in the buffer
+         * \param sample_age_out Return value, age of the returned sample (t_now - create_stamp)
+         */
+        void get_newest_sample(const uint64_t t_now, T& sample_out, uint64_t& sample_age_out)
+        {
+            std::lock_guard<std::mutex> lock(m_mutex);
+
+            sample_out = T();
+            sample_out.header().create_stamp().nanoseconds(0);
+            sample_age_out = t_now;
+
+            // select sample
+            for (auto& current_sample : messages_buffer)
+            {
+                if(current_sample.header().valid_after_stamp().nanoseconds() > t_now) 
+                {
+                    // Data is "in the future", ignore for now
+                    continue;
+                }
+
+                if(sample_out.header().create_stamp().nanoseconds() <= current_sample.header().create_stamp().nanoseconds())
+                {
+                    // Current sample has a higher timestamp, it is newer. Use it.
+                    sample_out = current_sample;
+                    sample_age_out = t_now - sample_out.header().valid_after_stamp().nanoseconds();
                 }
             }
         }
@@ -111,7 +136,7 @@ namespace cpm
             (dds::sub::qos::DataReaderQos() << dds::core::policy::History::KeepAll())
         )
         { 
-            static_assert(std::is_same<decltype(std::declval<T>().header().create_stamp().nanoseconds()), uint64_t>::value, "IDL type must have a Header.");
+            //static_assert(std::is_same<decltype(std::declval<T>().header().create_stamp().nanoseconds()), uint64_t>::value, "IDL type must have a Header.");
         }
         
         /**
@@ -124,7 +149,7 @@ namespace cpm
             (dds::sub::qos::DataReaderQos() << dds::core::policy::History::KeepAll())
         )
         { 
-            static_assert(std::is_same<decltype(std::declval<T>().header().create_stamp().nanoseconds()), uint64_t>::value, "IDL type must have a Header.");
+            //static_assert(std::is_same<decltype(std::declval<T>().header().create_stamp().nanoseconds()), uint64_t>::value, "IDL type must have a Header.");
         }
         
         /**
@@ -137,36 +162,14 @@ namespace cpm
          */
         void get_sample(const uint64_t t_now, T& sample_out, uint64_t& sample_age_out)
         {
-            std::lock_guard<std::mutex> lock(m_mutex);
-            flush_dds_reader(t_now);
+            flush_dds_reader();
 
-            sample_out = T();
-            sample_out.header().create_stamp().nanoseconds(0);
-            sample_age_out = t_now;
-
-            // select sample
-            for (int i = 0; i < CPM_READER_RING_BUFFER_SIZE; ++i)
-            {
-                auto& current_sample = ring_buffer[(i + ring_buffer_index) % CPM_READER_RING_BUFFER_SIZE];
-
-                if(current_sample.header().valid_after_stamp().nanoseconds() > t_now) 
-                {
-                    // Data is "in the future", ignore for now
-                    continue;
-                }
-
-                if(sample_out.header().create_stamp().nanoseconds() <= current_sample.header().create_stamp().nanoseconds())
-                {
-                    // Current sample has a higher timestamp, it is newer. Use it.
-                    sample_out = current_sample;
-                    sample_age_out = t_now - sample_out.header().valid_after_stamp().nanoseconds();
-                }
-            }
+            get_newest_sample(t_now, sample_out, sample_age_out);
 
             //Delete samples that are older than the selected sample (regarding valid_after)
             //TODO: At reviewer: Should messages that are too old regarding their creation stamp be deleted as well?
             //      If so: A 'timeout' for this could be set in the constructor
-            remove_old_msgs(sample_out.header().valid_after_stamp().nanoseconds());
+            remove_old_msgs(sample_out);
         }
     };
 
