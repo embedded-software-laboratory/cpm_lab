8ec075472806a09a2678768b904fcafebd6fe903
Patrick Scheffe <scheffe@embedded.rwth-aachen.de>
2020-05-04 11:41:12 +0200

    added node selection mode

--- matlab_scripts/sub_lane_graph/main.m
+++ matlab_scripts/sub_lane_graph/main.m
@@ -1,13 +1,20 @@
 function main
-
-    outer_circle_nodes = [3,  4,  6,  8,10, 56, 54, 51,52, 70, 72, 74,36, 34, 32, 30];
+    %% delete nodes
+    outer_circle_nodes = [3, 4, 6, 8, 10, 56, 54, 51, 52, 70, 72, 74, 36, 34, 32, 30];
     inner_cross_nodes = [37,  40, 28, 14, 11,57,  24, 68, 46, 75, 78,  66, 48, 23, 42,  60,  61, 27, 16, 17];
     comb = union(outer_circle_nodes, inner_cross_nodes);
     steep_curve_edges = [7 13; 58 55; 49 62; 79 69; 73 77; 38 33; 29 43; 19 2];
     
-    addpath('../map_print/map_print2')
+    addpath('../map_print/map_print2');
     
-    lanegraph = changed_graph(comb, steep_curve_edges) 
-    
-    lane_graph_to_cpp(lanegraph)
+    lane_graph = sub_graph_deletion(comb, steep_curve_edges);
+    visualize_lane_graph(lane_graph);
+    lane_graph_to_cpp(lane_graph)
+       
+    %% select nodes
+    addpath('../map_print/map_print2');
+    outer_circle_nodes = [3, 4, 6, 8, 10, 56, 54, 51, 52, 70, 72, 74, 36, 34, 32, 30];
+    lane_graph = sub_graph_selection(outer_circle_nodes);
+    visualize_lane_graph(lane_graph);
+    lane_graph_to_cpp(lane_graph);
 end
--- matlab_scripts/sub_lane_graph/sub_graph_deletion.m
+++ matlab_scripts/sub_lane_graph/sub_graph_deletion.m
@@ -1,17 +1,13 @@
 % Input del_nodes: Array of nodes to be deleted
 %       del_egdges: Array of node tuples [start,end] to be deleted
 % Output changed_graph.mat with lane_graph according to deletions
-function routing_graph = changed_graph(del_nodes, del_edges)
+function lane_graph = sub_graph_deletion(del_nodes, del_edges)
 
-    load('lane_graph.mat');
+    org = load('lane_graph.mat');
+    lane_graph = org.lane_graph;
     n_nodes = size(lane_graph.nodes, 1);
     n_edges = size(lane_graph.edges, 2);
-    
-    outer_circle_nodes = [3,  4,  6,  8,10, 56, 54, 51,52, 70, 72, 74,36, 34, 32, 30];
-    inner_cross_nodes = [37,  40, 28, 14, 11,57,  24, 68, 46, 75, 78,  66, 48, 23, 42,  60,  61, 27, 16, 17];
-    comb = union(outer_circle_nodes, inner_cross_nodes);
-    steep_curve_edges = [7 13; 58 55; 49 62; 79 69; 73 77; 38 33; 29 43; 19 2];
-    
+        
     
     set_nodes = [1:size(lane_graph.nodes, 1)];
     set_diff = setdiff(set_nodes, del_nodes);
@@ -49,9 +45,7 @@ function routing_graph = changed_graph(del_nodes, del_edges)
             end
         end
     end
-    
-    routing_graph = lane_graph
-    
+        
     n_nodes_after = size(lane_graph.nodes, 1);  
     
     figure
