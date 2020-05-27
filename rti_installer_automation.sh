e338f792b033e7a0b7e022aad23f82bec212cade
Alexandru Kampmann <kampmann@embedded.rwth-aachen.de>
2020-05-27 20:49:04 +0200

    * wip

--- rti_installer_automation.sh
+++ rti_installer_automation.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/expect
-set timeout 360
+set timeout 15
 spawn ./rti_connext_dds-6.0.0-eval-x64Linux4gcc7.3.0.run --prefix /opt/rti_connext_dds-6.0.0
 send "\r"
 send "\r"
@@ -11,3 +11,9 @@ send "\r"
 expect "Press \[Enter\] to continue"
 send "\r"
 send "\r"
+send "\r"
+send "\r"
+send "\r"
+send "\r"
+send "\r"
+expect "Disable copying of" { send "\r" }
