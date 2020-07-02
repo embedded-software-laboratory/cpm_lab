a7c62211e97e6909dba6fa96759272661383679b
Patrick Scheffe <scheffe@embedded.rwth-aachen.de>
2020-07-02 11:53:19 +0200

    WIP solve issues bc location not ~/dev/software

--- high_level_controller/autostart/build.bash
+++ high_level_controller/autostart/build.bash
@@ -2,6 +2,9 @@
 # exit when any command fails
 set -e
 
+# Get directory of bash script
+CURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 mkdir -p build
 cd build
 
@@ -12,8 +15,8 @@ cd ..
 # Publish autostart package via http/apache for the HLCs to download - TODO: Change tmp-version to local version after merge, similar to middleware build
 cd /tmp
 mkdir autostart_package
-cp ~/dev/software/high_level_controller/autostart/build/autostart ./autostart_package
-cp ~/dev/software/high_level_controller/autostart/build/download_error_logger ./autostart_package
+cp ${CURDIR}/build/autostart ./autostart_package
+cp ${CURDIR}/build/download_error_logger ./autostart_package
 tar -czvf autostart_package.tar.gz autostart_package
 rm -f /var/www/html/nuc/autostart_package.tar.gz
 cp ./autostart_package.tar.gz /var/www/html/nuc
--- high_level_controller/autostart/create_nuc_package.bash
+++ high_level_controller/autostart/create_nuc_package.bash
@@ -2,15 +2,19 @@
 # -> Only files that are probably relevant to all Matlab scripts are uploaded at this points
 # Scripts are still supposed to be uploaded using the LCC's UI (or your own upload script)
 
-# Publish NUC package via http/apache for the HLCs to download
-
 # exit when any command fails
 set -e
 
+# Get current directory
+CURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Publish NUC package via http/apache for the HLCs to download
+
+
 cd /tmp
 mkdir matlab_package
-cp ~/dev/software/high_level_controller/matlab/init_script.m ./matlab_package
-cp ~/dev/software/high_level_controller/matlab/QOS_READY_TRIGGER.xml ./matlab_package
+cp ${CURDIR}/../examples/matlab/init_script.m ./matlab_package
+cp ${CURDIR}/../examples/matlab/QOS_READY_TRIGGER.xml ./matlab_package
 tar -czvf matlab_package.tar.gz matlab_package
 rm -f /var/www/html/nuc/matlab_package.tar.gz
 cp ./matlab_package.tar.gz /var/www/html/nuc
