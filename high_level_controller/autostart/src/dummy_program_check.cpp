b150c7e196d8cecd4c3cf7280e232582d4dd9ca6
Leon <leon.rabanus@rwth-aachen.de>
2020-08-26 13:33:16 +0200

    Updated implementation suggestion for LCC

--- high_level_controller/autostart/src/dummy_program_check.cpp
+++ high_level_controller/autostart/src/dummy_program_check.cpp
@@ -30,7 +30,9 @@
  */
 
 #include <chrono>
+#include <map>
 #include <memory>
+#include <mutex>
 #include <sstream>
 #include <string>
 #include <functional>
@@ -64,12 +66,18 @@ int main (int argc, char *argv[]) {
     cpm::init(argc, argv);
     cpm::Logging::Instance().set_id("dummy_check"); 
 
+    //These data structures will later be used in the LCC to regularly check if the remotely deployed program is still online
+    std::mutex map_mutex;
+    std::map<std::string, bool> script_running;
+    std::map<std::string, bool> middleware_running;
+
     //Create reader and writer to check HLC answers
     dds::topic::Topic<RemoteProgramCheck> program_check_topic = cpm::get_topic<RemoteProgramCheck>(cpm::ParticipantSingleton::Instance(), "remote_program_check");
     dds::pub::DataWriter<RemoteProgramCheck> program_check_writer(dds::pub::Publisher(cpm::ParticipantSingleton::Instance()), program_check_topic, (dds::pub::qos::DataWriterQos() << dds::core::policy::Reliability::Reliable() << dds::core::policy::History::KeepAll()));
 
     cpm::AsyncReader<RemoteProgramCheck> program_check_reader(
         [&](dds::sub::LoanedSamples<RemoteProgramCheck>& samples){
+            std::lock_guard<std::mutex> lock(map_mutex);
             for(auto sample : samples)
             {
                 if(sample.info().valid())
@@ -77,11 +85,8 @@ int main (int argc, char *argv[]) {
                     //Only print answers
                     if ((sample.data().is_answer()))
                     {
-                        std::cout << "Received answer: \n"
-                            << "\tSource ID: " << sample.data().source_id()
-                            << "\tScript running: " << sample.data().script_running()
-                            << "\tMiddleware running: " << sample.data().middleware_running()
-                            << std::endl;
+                        script_running[sample.data().source_id()] = sample.data().script_running();
+                        middleware_running[sample.data().source_id()] = sample.data().middleware_running();
                     }
                 }
             }
@@ -100,14 +105,62 @@ int main (int argc, char *argv[]) {
     check_msg.source_id("dummy_check");
     check_msg.is_answer(false);
 
-    for (uint8_t i = 0; i < 255; ++i)
+    //Again dummy code for later implementation in the LCC
+    std::vector<uint8_t> running_hlc_ids = { 1, 3, 8 }; //Will later be dynamic, depending on which HLCs the software was deployed
+    //The vector should maybe also be updated if a NUC went offline/crashed, but reporting this twice would also not be too bad 
+    //(Report would then be: Programs XY have crashed, NUC crashed -> probably even better this way)
+
+
+    for (uint8_t i = 0; i < 255; ++i) //LCC: Make this an infinite loop within a thread that can be stopped from outside (with an atomic variable)
     {
         check_msg.count(i);
         program_check_writer.write(check_msg);
         std::cout << "Msg written" << std::endl;
 
-        //Wait for replies & print them in DDS thread
+        //Wait for replies before sending the next msg
         std::this_thread::sleep_for(std::chrono::seconds(2));
+
+        //Check for answers for all IDs, in LCC: make sure that some time has passed before missing answers are being reported 
+        //(from testing experience: in the beginning, the connection is not stable enough to assume that the other party is offline,
+        //and there are tests for offline NUCs already)
+        for (auto id_iterator = running_hlc_ids.begin(); id_iterator != running_hlc_ids.end();)
+        {
+            auto id_string = std::to_string(static_cast<int>(*id_iterator));
+
+            std::lock_guard<std::mutex> lock(map_mutex);
+            if (script_running.find(id_string) != script_running.end())
+            {
+                //Entry exists in both maps, check if the script and the middleware are still running
+                if (middleware_running.at(id_string) && script_running.at(id_string))
+                {
+                    ++id_iterator;
+                }
+                else
+                {
+                    if (! script_running.at(id_string))
+                    {
+                        cpm::Logging::Instance().write("Script crashed on NUC %s (remote)", id_string.c_str());
+                    }
+
+                    if (! middleware_running.at(id_string))
+                    {
+                        cpm::Logging::Instance().write("Middleware crashed on NUC %s (remote)", id_string.c_str());
+                    }
+
+                    //Erase entry s.t. the message does not appear again
+                    id_iterator = running_hlc_ids.erase(id_iterator);
+                }
+
+                //Delete entry s.t. we do not assume next time that we received an answer
+                script_running.erase(id_string);
+                middleware_running.erase(id_string);
+            }
+            else
+            {
+                cpm::Logging::Instance().write("NUC %s did not respond to program online check", id_string.c_str());
+                //Maybe actually remove IDs of offline NUCs to prevent confusion regarding this msg
+            }
+        }
     }
 
     #pragma GCC diagnostic pop
