4c92bcd409d0c701f259b411298940c91515a649
Marius Hasselbach <marius.hasselbach@rwth-aachen.de>
2020-11-27 08:31:46 +0100

    use [ -z $SIMULATION ] instead of [ $SIMULATION == 0 ] to build non simulation only code

--- build_all.bash
+++ build_all.bash
@@ -4,21 +4,19 @@ set -e
 # Get directory of bash script
 BASH_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ${BASH_DIR}
-#Get command line arguments
-SIMULATION=0
-HEADLESS=0
 
+#Get command line arguments
 for arg in "$@"
 do
     case $arg in
         -s|--simulation)
-        SIMULATION=1
-	    echo "Building in simulation mode: no IPS, no ARM builds."
+        export SIMULATION=1
+	    echo -e "\e[32m Building in simulation mode: no IPS, no ARM builds. \e[0m"
         shift # Remove --simulation from processing
         ;;
         --headless)
-        HEADLESS=1
-	    echo "Building in headless mode: no LCC."
+        export HEADLESS=1
+	    echo -e "\e[32m Building in headless mode: no LCC. \e[0m"
         shift # Remove --headless from processing
         ;;
         *)
@@ -27,26 +25,35 @@ do
     esac
 done
 
-#make simulation variable available in all bash scripts called
-export SIMULATION
+# warn users who still use SIMULATION=0
+if [ ! -z $SIMULATION ]; then
+    if [ $SIMULATION == 0 ]; then
+        echo -e "\e[31m WARNING: Deprecated ussage of shell variable SIMULATION detected. \e[0m"
+        echo -e "\e[31m          SIMULATION=0 does not work anymore. Please use: \e[0m"
+        echo -e "\e[31m          - SIMULATION=1 for simulation only builds \e[0m"
+        echo -e "\e[31m          - no SIMULATION variable ('unset SIMULATION') for standard builds \e[0m"
+        unset SIMULATION
+        echo -e "\e[32m SIMULATION=0 has been unset for you. \e[0m"
+    fi
+fi
 
 # cpm lib
 pushd cpm_lib
-    if [ $SIMULATION == 0 ]; then
+    if [ -z $SIMULATION ]; then
         bash build_arm.bash  
     fi
-    bash build.bash
+    bash build.bash     # checks for $SIMULATION by itself
 popd
 
 # lcc
-if [ $HEADLESS == 0 ]; then
+if [ -z $HEADLESS ]; then
     pushd lab_control_center
         bash build.bash
     popd
 fi
 
 pushd high_level_controller
-    if [ $SIMULATION == 0 ]; then
+    if [ -z $SIMULATION ]; then
         pushd autostart
             bash build.bash
             bash create_nuc_package.bash
@@ -70,20 +77,15 @@ pushd high_level_controller
 popd
 
 pushd middleware
-    bash build.bash
+    bash build.bash     # checks for $SIMULATION by itself
 popd
 
 pushd mid_level_controller
-    if [ $SIMULATION == 0 ]; then
-        bash build.bash
-    else
-        bash build.bash --simulation
-    fi
+    bash build.bash     # checks for $SIMULATION by itself
 popd
 
-if [ $SIMULATION == 0 ]; then
+if [ -z $SIMULATION ]; then
     pushd indoor_positioning_system
         bash build.bash
     popd
 fi
-
