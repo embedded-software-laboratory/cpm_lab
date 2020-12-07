2442c9d4817abd04990259509dd2afa313e0e7bc
leon <leon>
2020-12-07 18:28:00 +0100

    Final sync changes & reliable msgs

--- high_level_controller/autostart/time_sync_test.bash
+++ high_level_controller/autostart/time_sync_test.bash
@@ -1,39 +1,20 @@
-# Ping to make sure that an internet connection is available
-# Write to /dev/null to suppress output
-until ping -c1 ntp1.rwth-aachen.de >/dev/null 2>&1; do sleep 0.1; done
+#!/bin/bash
 
-# Now enforce time sync
-sudo service ntp stop
-sudo ntpd -gq
-sudo service ntp restart
-
-# If you do not want sudo: Place in /etc/rc.local
+# Place in /etc/rc.local
 # The service only starts if the file is formatted properly, like this, with your code in between:
-
-#!/bin/sh
-#
-# rc.local
-#
-# Some explanation
-
-# Your code
-
-exit 0
 
 # Also, you need to make sure that the file is executable: sudo chmod +x /etc/rc.local
 
 # Another problem: We want the lab / hlc program to only start if the time sync was finished
-# Solutions for the rc.local as well as lab_autostart script can be found below:
+# This is done by using pipes between lab_autostart and the enforced time sync in rc.local
 
 # For communication between processes, proposed solution:
 # -------------------------------------------------------
 # -------------------------------------------------------
 # -------------------------------------------------------
 
-#!/bin/bash
-
 # First, set a wrong date (for testing purposes)
-date +%T -s "14:14:00"
+# date +%T -s "14:14:00"
 
 # Ping to make sure that an internet connection is available
 # Write to /dev/null to suppress output
@@ -72,32 +53,9 @@ while read ans < $nuc_lab_pipe; do
         sleep 1
 done
 
+# Also just for testing purposes
 echo "Done" >> /tmp/test.txt
 
 exit 0
 
-# Proposed change for autostart, add at start after export statements:
-# -------------------------------------------------------
-# -------------------------------------------------------
-# -------------------------------------------------------
-
-# Wait for clock sync before doing anything else, because starting any program before a clock sync would cause problems
-# The clock sync is performed in rc.local of the sudo user
-# We communicate via pipes, alternatively starting this script as another user would have been possible as well
-# Check for existing pipe, wait until it has been created
-nuc_ntp_pipe=/tmp/nuc_ntp_pipe
-nuc_lab_pipe=/tmp/nuc_lab_pipe
-
-while [[ ! (-p $nuc_ntp_pipe && -p $nuc_lab_pipe ) ]]; do
-        sleep 1
-done
-
-# Read msg from pipe, which is sent after the time sync, then answer
-# 1 -> time sync done, answer with 1 on other pipe
-while read sy < $nuc_ntp_pipe; do
-	if [[ $sy -eq 1 ]]; then
-		break
-	fi
-	sleep 1
-done
-echo "1" > $nuc_lab_pipe
+# The answer is given by lab_autostart, which is already automatically started (see Documentation)
