1a744e406ec01f6c450dd909ccf23dd7f5df220f
Leon <leon.rabanus@rwth-aachen.de>
2021-01-25 13:40:55 +0100

    Added docs for timer UI

--- lab_control_center/ui/timer/TimerModelRecord.hpp
+++ lab_control_center/ui/timer/TimerModelRecord.hpp
@@ -37,6 +37,11 @@
  */
 class TimerModelRecord : public Gtk::TreeModelColumnRecord {
 public:
+  /**
+   * \brief Constructor
+   * 
+   * Sets the entries / columns for the TreeView that uses this record, here Timer ID | Time since last message | Participant status | Next time step
+   */
   TimerModelRecord() { 
       add(column_id);
       add(column_last_message);
@@ -44,8 +49,12 @@ public:
       add(column_next_step);
     }
 
+  //! Timer ID, column of the TreeView that uses this record
   Gtk::TreeModelColumn<Glib::ustring> column_id;
+  //! Time since last message from this timer instance, column of the TreeView that uses this record
   Gtk::TreeModelColumn<Glib::ustring> column_last_message;
+  //! Status of the timer / participant with the given ID, column of the TreeView that uses this record
   Gtk::TreeModelColumn<Glib::ustring> column_participant_status;
+  //! Next time step of the timer (for simulated time), column of the TreeView that uses this record
   Gtk::TreeModelColumn<Glib::ustring> column_next_step;
 };
