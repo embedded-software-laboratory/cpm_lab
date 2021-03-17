22eb7d2fee5e8dc74ada7b2eddbd75d9f774491d
Maximilian Kloock <kloock@embedded.rwth-aachen.de>
2021-03-17 14:05:41 +0000

    Fix from middleware

--- high_level_controller/examples/matlab/QP_commonroad_target/main_vehicle_ids.m
+++ high_level_controller/examples/matlab/QP_commonroad_target/main_vehicle_ids.m
@@ -10,7 +10,7 @@ function main_vehicle_ids(varargin)
     init_script_path = fullfile('../', '/init_script.m');
     assert(isfile(init_script_path), 'Missing file "%s".', init_script_path);
     addpath(fileparts(init_script_path));
-    [matlabParticipant, stateReader, trajectoryWriter, systemTriggerReader, readyStatusWriter, trigger_stop] = init_script(matlabDomainID);
+    [matlabParticipant, stateReader, trajectoryWriter, ~, systemTriggerReader, readyStatusWriter, trigger_stop] = init_script(matlabDomainID);
     cd(script_directory)
 
 
