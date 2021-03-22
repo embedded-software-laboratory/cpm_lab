136f6c6a979beb77b3c11de98762a32affec2c7b
Johannes Rolf <johannes.rolf@rwth-aachen.de>
2021-03-22 17:18:28 +0100

    Mistake in run.bash of example eight_zero corrected

--- high_level_controller/examples/cpp/eight_zero/run.bash
+++ high_level_controller/examples/cpp/eight_zero/run.bash
@@ -8,8 +8,5 @@ if [ $# -eq 0 ]
   then
     echo "Missing argument vehicle ID"
 else
-  echo $DDS_DOMAIN
-  echo $DDS_INITIAL_PEER
-  echo $1
-	./build/eight_zero --dds_domain=$DDS_DOMAIN --dds_initial_peer=$DDS_INITIAL_PEER --vehicle_id=$1
+	./build/eight_zero --dds_domain=$DDS_DOMAIN --dds_initial_peer=$DDS_INITIAL_PEER --vehicle_ids=$1
 fi
