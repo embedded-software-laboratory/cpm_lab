5ce83bc415078acfd9c79e43bcd6d4cbcb7b8881
Leon <leon.rabanus@rwth-aachen.de>
2021-03-22 10:15:16 +0100

    Made participant creation thread-safe

--- cpm_lib/include/cpm/ParticipantSingleton.hpp
+++ cpm_lib/include/cpm/ParticipantSingleton.hpp
@@ -27,6 +27,7 @@
 #pragma once
 
 #include <dds/domain/DomainParticipant.hpp>
+#include <mutex>
 
 namespace cpm 
 {
@@ -47,6 +48,9 @@ namespace cpm
      */
     class ParticipantSingleton
     {
+    private:
+        //! The instance creation is not atomic. Thus: Use a mutex to make sure that the Singleton is not created twice.
+        static std::mutex creation_mutex;
     public:
         ParticipantSingleton() = delete;
         ParticipantSingleton(ParticipantSingleton const&) = delete;
