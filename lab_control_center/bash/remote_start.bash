d80b460be1e810c75d823476d28ea14fbc862c90
leon <leon>
2021-03-29 12:48:27 +0200

    Reverted wrong NUC script changes

--- lab_control_center/bash/remote_start.bash
+++ lab_control_center/bash/remote_start.bash
@@ -23,20 +23,16 @@ case $i in
 esac
 done
 
-# Get log directory relative to this script's directory
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/"
-RELATIVE_LOG_DIR="${DIR}../../../lcc_script_logs"
-ABSOLUTE_LOG_DIR="$(realpath "${RELATIVE_LOG_DIR}")"
-
-# Kill potential previous sessions - DO NOT change these session names, unless you intend to change them in the whole software repo
+# Kill potential previous sessions
 tmux kill-session -t "middleware"
 tmux kill-session -t "script"
 
-# Clear previous log files, no longer done
-# rm -rf '${ABSOLUTE_LOG_DIR}';mkdir -p '${ABSOLUTE_LOG_DIR}'
+
+# Kill potential previous sessions - DO NOT change these session names, unless you intend to change them in the whole software repo
+rm -rf ~/dev/lcc_script_logs;mkdir -p ~/dev/lcc_script_logs
 
 # Start middleware
-tmux new-session -d -s "middleware" "cd /tmp/scripts/;bash tmux_middleware.bash --middleware_arguments='${MIDDLEWARE_ARGS}' &> '${ABSOLUTE_LOG_DIR}'/tmux_middleware.txt"
+tmux new-session -d -s "middleware" "cd /tmp/scripts/;bash tmux_middleware.bash --middleware_arguments='${MIDDLEWARE_ARGS}' &> ~/dev/lcc_script_logs/tmux_middleware.txt"
 
 # Start script
-tmux new-session -d -s "script" "cd /tmp/scripts/;bash tmux_script.bash --script_path=${SCRIPT_PATH} --script_arguments='${SCRIPT_ARGS}' &> '${ABSOLUTE_LOG_DIR}'/tmux_script.txt"
+tmux new-session -d -s "script" "cd /tmp/scripts/;bash tmux_script.bash --script_path=${SCRIPT_PATH} --script_arguments='${SCRIPT_ARGS}' &> ~/dev/lcc_script_logs/tmux_script.txt"
