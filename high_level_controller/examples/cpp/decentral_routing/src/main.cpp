cbd27a6c2206316f40d57a978e9eeb58f2261753
Ole Gress <gress@embedded.rwth-aachen.de>
2021-04-21 11:34:49 +0200

    Refactored decentral_routing
    We can reuse the MiddlewareListener (name WIP) for other HLCs as well,
    to make it easier to use the middleware.

--- high_level_controller/examples/cpp/decentral_routing/src/main.cpp
+++ high_level_controller/examples/cpp/decentral_routing/src/main.cpp
@@ -60,6 +60,7 @@
 #include "CouplingGraph.hpp"
 // Planner
 #include "VehicleTrajectoryPlanner.hpp"    //sw-folder central routing
+#include "MiddlewareListener.hpp"
 
 using std::vector;
 
@@ -160,11 +161,6 @@ int main(int argc, char *argv[]) {
 
     std::cout << vehicle_id_string << std::endl;
 
-    const bool iterative_planning_enabled = false;
-
-    // SystemTrigger value that means "stop" (as defined in SystemTrigger.idl)
-    const uint64_t trigger_stop = std::numeric_limits<uint64_t>::max();
-
     // FIXME: Dirty hack to get our QOS settings
     // On the NUC we only have the QOS File for the middleware
     // and RTI DDS doesn't want to load it from there, so we copy it to our working dir.
@@ -174,44 +170,11 @@ int main(int argc, char *argv[]) {
 
     // Initialize everything needed for communication with middleware
     const int middleware_domain = cpm::cmd_parameter_int("middleware_domain", 1, argc, argv);
-    //dds::core::QosProvider local_comms_qos_provider("./QOS_LOCAL_COMMUNICATION.xml", "MatlabLibrary::LocalCommunicationProfile");
-    //dds::domain::DomainParticipant local_comms_participant(
-    //        middleware_domain,
-    //        local_comms_qos_provider.participant_qos()
-    //);
+
     cpm::Participant local_comms_participant(
             middleware_domain,
             "./QOS_LOCAL_COMMUNICATION.xml",
-            "MatlabLibrary::LocalCommunicationProfile"
-    );
-
-    /* --------------------------------------------------------------------------------- 
-     * Create readers and writers for communication with middleware
-     * ---------------------------------------------------------------------------------
-     */
-    // These QoS Settings are taken from the QOS_READY_TRIGGER.xml used in matlab example
-    cpm::Writer<ReadyStatus> writer_readyStatus(
-            local_comms_participant.get_participant(),
-            "readyStatus",
-            true,
-            true,
-            true
-    );
-
-    // systemTrigger Reader, QoS Settings taken from QOS_READY_TRIGGER.xml
-    cpm::ReaderAbstract<SystemTrigger> reader_systemTrigger(
-            local_comms_participant.get_participant(),
-            "systemTrigger",
-            true,
-            true
-    );
-
-    // VehicleStateList is our timing signal from the middleware
-    cpm::ReaderAbstract<VehicleStateList> reader_vehicleStateList(
-            local_comms_participant.get_participant(),
-            "vehicleStateList"
-    );
-     
+            "MatlabLibrary::LocalCommunicationProfile");
     // Writer to send trajectory to middleware
     cpm::Writer<VehicleCommandTrajectory> writer_vehicleCommandTrajectory(
             local_comms_participant.get_participant(),
@@ -249,57 +212,14 @@ int main(int argc, char *argv[]) {
         )
     );
 
-    /* ---------------------------------------------------------------------------------
-     * Compose and send Ready message
-     * ---------------------------------------------------------------------------------
-     */
-    // FIXME: Why do we need this 5 seconds wait? We do, but why and what would be a better solution?
-    // We could replace this by checking for how many other DDS participants we found. But are we 100% sure how many we need to find?
-    std::this_thread::sleep_for(std::chrono::seconds(5));
-    // Create arbitrary timestamp as per ReadyStatus.idl
-    TimeStamp timestamp(11111);
-    // The middleware expects a message like "hlc_${vehicle_id}", e.g. hlc_1
-    std::string hlc_identification("hlc_");
-    hlc_identification.append(std::to_string(vehicle_id));
-    ReadyStatus readyStatus(hlc_identification, timestamp);
-    writer_readyStatus.write(readyStatus);
+    MiddlewareListener middleware_listener(
+            vehicle_id,
+            middleware_domain,
+            "./QOS_LOCAL_COMMUNICATION.xml",
+            "MatlabLibrary::LocalCommunicationProfile");
 
-    /* ---------------------------------------------------------------------------------
-     * main loop to regularly send commands
-     * ---------------------------------------------------------------------------------
-     */
-    bool matched = false; // Status if we initialized vehicle on the LaneGraph
-    bool planning = false; // Status if we are currently planning
-    bool new_vehicleStateList = false; // If we received a new VehicleStateList
-    bool stop = false; // Exit from main loop
-    VehicleStateList vehicleStateList;
-    std::future<std::unique_ptr<VehicleCommandTrajectory>> cmd_future;
-
-#if TIMED
-    auto start_time = std::chrono::steady_clock::now();
-    auto end_time = std::chrono::steady_clock::now();
-#endif
-
-    while( !stop ) {
-        auto state_samples = reader_vehicleStateList.take();
-        for(auto sample : state_samples) {
-            // We received a StateList, which is our timing signal
-            // to send commands to vehicle
-            new_vehicleStateList = true;
-            vehicleStateList = sample;
-        }
-
-        // The first time we receive a sample, we need to initialize our position on the laneGraph
-        if(new_vehicleStateList && !planner->is_started()) {
-            cpm::Logging::Instance().write(3,
-                "Preparing to start planner");
-
-            /* ---------------------------------------------------------------------
-            * Check for start position of our vehicle
-            * ----------------------------------------------------------------------
-            */
-            // This probably does not require a loop,
-            // we just need to find the one VehicleState with our vehicle_id
+    middleware_listener.setOnFirstTimestep([&](VehicleStateList vehicleStateList){
+            bool matched = false;
             for(auto vehicle_state : vehicleStateList.state_list())
             {
                 if( vehicle_id == vehicle_state.vehicle_id() ) {
@@ -310,7 +230,7 @@ int main(int argc, char *argv[]) {
                     if( !matched ) {
                         cpm::Logging::Instance().write(1,
                             "Couldn't find starting position,\
-                            try moving the vehicle if this persists.");
+                            try moving the vehicle.");
                     } else {
 
                         // This graphs gives the priorities, as well as the order of planning
@@ -342,100 +262,24 @@ int main(int argc, char *argv[]) {
                     }
                 }
             }
-
-            if( !matched ) {
-                // We might want to stop everything here, but it happens too
-                // often for that
-                cpm::Logging::Instance().write(2,
-                    "Couldn't find vehicle in VehicleStateList.");
-            }
-        }
-
-        if( new_vehicleStateList && matched ) {
-            // We are using the stateList now, so it isn't new anymore
-            new_vehicleStateList = false;
-
-            // Stop planning of previous timestep, if necessary
-            if(planning) {
-#if TIMED
-                end_time = std::chrono::steady_clock::now();
-                auto diff = end_time - start_time;
-                std::cout
-                    << "TIMING: Aborting planning after "
-                    << std::chrono::duration<double, std::milli>(diff).count()
-                    << " ms"
-                    << std::endl;
-#endif
-                planner->stop();
-                planning = false;
-            }
-
-#if TIMED
-            std::cout
-                << "------------- Timestep "
-                << vehicleStateList.t_now()
-                << " -------------"
-                << std::endl;
-            start_time = std::chrono::steady_clock::now();
-#endif
-            // Start async job for planning
-            cmd_future = std::async(std::launch::async,
-                    [&]{
-                        return planner->plan(vehicleStateList.t_now(), vehicleStateList.period_ms()*1e6);
-                    }
-                );
-            planning = true;
-        }
-
-        // Check status of async planning
-        if( planning && cmd_future.valid() ) {
-            // Waits 1ms for result (or just get it instantly?)
-            std::future_status future_status = cmd_future.wait_for(std::chrono::milliseconds(1));
-            if( future_status == std::future_status::ready ) {
-#if TIMED
-                end_time = std::chrono::steady_clock::now();
-                auto diff = end_time - start_time;
-                std::cout
-                    << "TIMING: Finished planning after "
-                    << std::chrono::duration<double, std::milli>(diff).count()
-                    << " ms"
-                    << std::endl;
-#endif
-                // Get commands and send to vehicle
-                std::unique_ptr<VehicleCommandTrajectory> cmd = cmd_future.get();
-                if( cmd.get() != nullptr ) {
-                    writer_vehicleCommandTrajectory.write(*cmd.get());
+            });
+    middleware_listener.setOnEachTimestep([&](VehicleStateList vehicleStateList){
+                auto trajectory = planner->plan(vehicleStateList.t_now(), vehicleStateList.period_ms()*1e6);
+                if( trajectory.get() != nullptr ) {
+                    writer_vehicleCommandTrajectory.write(*trajectory.get());
                 } else {
                     cpm::Logging::Instance().write(2,
-                            "Unexpected missing value from planner");
+                            "Planner didn't return a value this timestep");
                 }
-                planning = false;
-            }
-        }
+            });
+    middleware_listener.setOnCancelTimestep([&]{
+                planner->stop(); 
+            });
+    middleware_listener.setOnStop([&]{
+                planner->stop(); 
+            });
 
-        // Check if we received a SystemTrigger to stop
-        auto systemTrigger_samples = reader_systemTrigger.take();
-        for (auto sample : systemTrigger_samples) {
-            if (sample.next_start().nanoseconds() == trigger_stop) {
-                cpm::Logging::Instance().write(
-                    2,
-                    "Received stop signal, stopping"
-                    );
-                    stop = true;
-            }
-        }
-
-        // Check if the planner encountered a problem
-        if( planner->is_crashed() ) {
-            stop = true;
-            StopRequest request(vehicle_id);
-            writer_stopRequest.write(request);
-        }
-
-        // Rate limit main while loop to 10-times per dt_nanos
-        // Arbitrary, but larger waits should decrease cpu load of this loop
-        std::this_thread::sleep_for(std::chrono::nanoseconds(vehicleStateList.period_ms()/10));
-    }
+    middleware_listener.start();
 
     return 0;
 }
