7ea99343adcf5678f2ab7bbcfc6e560a5d804f04
Georg Dorndorf <georg.dorndorf@rwth-aachen.de>
2022-01-25 13:10:57 +0100

    Cleanup scripts

--- high_level_controller/examples/cpp/dynamic_priorities/run.bash
+++ high_level_controller/examples/cpp/dynamic_priorities/run.bash
@@ -4,9 +4,5 @@
 export IP_SELF=$(ip route get 8.8.8.8 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}')
 export DDS_INITIAL_PEER=rtps@udpv4://$IP_SELF:25598
 
-if [ $# -eq 0 ]
-  then
-    echo "Missing argument vehicle ID list (comma separated)"
-else
-	./build/dynamic_priorities --dds_domain=$DDS_DOMAIN --dds_initial_peer=$DDS_INITIAL_PEER --vehicle_ids=2
-fi
+# only one vehicle id is to be specified as the hlc has to be run distributedly
+./build/dynamic_priorities  --vehicle_ids=1 --hlc_mode=2
--- high_level_controller/examples/cpp/dynamic_priorities/run_simulation.bash
+++ high_level_controller/examples/cpp/dynamic_priorities/run_simulation.bash
@@ -4,5 +4,7 @@
 export IP_SELF=$(ip route get 8.8.8.8 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}')
 export DDS_INITIAL_PEER=rtps@udpv4://$IP_SELF:25598
 
+# heptrack can be used to analyse memory usage and detect leaks
+# heaptrack ./build/simulation --n=5 --hlc_mode=2 --steps=100 
 
-heaptrack ./build/simulation --dds_domain=$DDS_DOMAIN --dds_initial_peer=$DDS_INITIAL_PEER --vehicle_ids=1,2,3,4,5 --hlc_mode=2
+./build/simulation --n=15 --hlc_mode=2
