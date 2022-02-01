e4aa73db6dfaa2cc9dcc3bf36273805a0c36512f
Georg Dorndorf <georg.dorndorf@rwth-aachen.de>
2022-02-01 14:56:18 +0100

    Update eval skript

--- high_level_controller/examples/cpp/dynamic_priorities/eval.sh
+++ high_level_controller/examples/cpp/dynamic_priorities/eval.sh
@@ -2,17 +2,23 @@
 # It places them in the folder ./data/MODE/N/
 # It currently uses the planning horizon which is hardcoded in the HLC. To generate data for different planning horizons the value of  N_STEPS_SPEED_PROFILE has to be changed in VehicleTrajectoryPlanningState.hpp.
 
+HORIZON="200"
+SEED=4
+
+echo $SEED
 mkdir data
 cd ./data
-for MODE in  0
+mkdir 200 # horizon in timesteps 
+cd 200
+for MODE in  0 1 2
 do
     mkdir $MODE
     cd $MODE
-    for N in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18
+    for N in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
     do
         mkdir $N
         cd ./$N
-        ../../../build/simulation --n=$N --hlc_mode=$MODE >> cout.log
+        ../../../../build/simulation --n=$N --hlc_mode=$MODE --path_seed=$SEED --prio_seed=$SEED >> cout.log
         cd ..
     done
     cd ..
