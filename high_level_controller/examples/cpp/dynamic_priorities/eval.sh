1f02c229f2f4ea09c88e00bf91c1b224e0139dc4
Patrick Scheffe <patrick.scheffe@rwth-aachen.de>
2022-02-01 16:32:28 +0100

    seed loop

--- high_level_controller/examples/cpp/dynamic_priorities/eval.sh
+++ high_level_controller/examples/cpp/dynamic_priorities/eval.sh
@@ -3,24 +3,28 @@
 # It currently uses the planning horizon which is hardcoded in the HLC. To generate data for different planning horizons the value of  N_STEPS_SPEED_PROFILE has to be changed in VehicleTrajectoryPlanningState.hpp.
 
 HORIZON="200"
-SEED=4
+SEEDSTART=101
+SEEDEND=110
 
-echo $SEED
-mkdir data
-cd ./data
-mkdir 200 # horizon in timesteps 
-cd 200
-for MODE in  0 1 2
+for ((SEED=SEEDSTART;SEED<=SEEDEND;SEED++))
 do
-    mkdir $MODE
-    cd $MODE
-    for N in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+    mkdir data_seed_${SEED}
+    cd ./data_seed_${SEED}
+    mkdir -p 200 # horizon in timesteps 
+    cd 200
+    for MODE in  0 1 2
     do
-        mkdir $N
-        cd ./$N
-        ../../../../build/simulation --n=$N --hlc_mode=$MODE --path_seed=$SEED --prio_seed=$SEED >> cout.log
+        mkdir -p $MODE
+        cd $MODE
+        for N in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+        do
+            mkdir $N
+            cd ./$N
+            ../../../../build/simulation --n=$N --hlc_mode=$MODE --path_seed=$SEED --prio_seed=$SEED >> /dev/null
+            cd ..
+        done
         cd ..
     done
     cd ..
+    cd ..
 done
-
